4c3baac3e92f1b08d5b210a9a79af5be27806bb9
Neale Taylor <nealeptaylor@gmail.com>
2022-03-17 18:30:52 -0400

    add cors

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios.get('http://localhost:8080/users').then(res => {
+      console.log(res);
+    })
+  }, [])
+
   return (
     <div className="App">
-
+      <h1>Hello World</h1>
     </div>
   );
 }
