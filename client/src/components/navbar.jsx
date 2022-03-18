3f0467502d6a4adca26ef8ad21236efc3639f15f
Vivian Zhang <vvynz12@gmail.com>
2022-03-18 19:37:45 -0400

    Add links to future routes

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -7,8 +7,11 @@ export default function Navbar() {
     <nav>
       <div className="top-nav">
         <h1 className="nav-tab">Project Tracker</h1>
+        {/* <Link to="/summary" className="nav-tab">Summary</Link> */}
         <span className="nav-tab">Summary</span>
+        {/* <Link to="/schedule" className="nav-tab">Schedule</Link> */}
         <span className="nav-tab">Schedule</span>
+
         <span className="login-nav">
           <Link to="/register">Register</Link>
           <Link to="/login">Login</Link>
