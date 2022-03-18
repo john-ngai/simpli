6ebee974dfc57ab80b85961749314e137c9f5774
Vivian Zhang <vvynz12@gmail.com>
2022-03-18 18:39:51 -0400

    Import NavBar component into app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import {React, useState, useEffect} from 'react';
+import { React, useState, useEffect } from 'react';
 import axios from 'axios';
 import useAppData from './hooks/useAppData';
 import ProjectList from './components/ProjectList';
+import NavBar from './components/NavBar';
 
 function App() {
   const { state } = useAppData();
@@ -13,27 +14,30 @@ function App() {
   // })
 
   return (
-    <main className="layout">
-      <section className="projects">
-        <nav>
-          <ProjectList 
-            projects={state.projects}
-          />
-        </nav>
-      </section>
-      <section className="deliverables">
-        <nav>
-          <li className="deliverable_list_item">
-            <span className="deliverable_name">Clean master bedroom</span>
-            <span className="deliverable_description"></span>
-          </li>
-          <li className="deliverable_list_item">
-            <span className="deliverable_name">Clean master bedroom</span>
-            <span className="deliverable_description"></span>
-          </li>
-        </nav>
-      </section>
-    </main>
+    <div>
+      <NavBar />
+      <main className="layout">
+        <section className="projects">
+          <nav>
+            <ProjectList
+              projects={state.projects}
+            />
+          </nav>
+        </section>
+        <section className="deliverables">
+          <nav>
+            <li className="deliverable_list_item">
+              <span className="deliverable_name">Clean master bedroom</span>
+              <span className="deliverable_description"></span>
+            </li>
+            <li className="deliverable_list_item">
+              <span className="deliverable_name">Clean master bedroom</span>
+              <span className="deliverable_description"></span>
+            </li>
+          </nav>
+        </section>
+      </main>
+    </div>
   );
 }
 
