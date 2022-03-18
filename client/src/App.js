721570300f30a0fcc460a0f133047e7c09135a1a
mrjohnming <john.ngai@hotmail.com>
2022-03-18 17:58:40 -0400

    Add temp links - /register & /login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import {React, useState, useEffect} from 'react';
+import { React, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import useAppData from './hooks/useAppData';
 import ProjectList from './components/ProjectList';
@@ -16,9 +17,24 @@ function App() {
     <main className="layout">
       <section className="projects">
         <nav>
-          <ProjectList 
+          <ProjectList
             projects={state.projects}
           />
+        </nav>
+        <nav>
+          {/* REMINDER: Remove test code */}
+          <li className="project_list_item">
+            <span className="project_name">
+              <Link to="/register">Register</Link>
+            </span>
+          </li>
+
+          <li className="project_list_item">
+            <span className="project_name">
+              <Link to="/login">Login</Link>
+            </span>
+          </li>
+          {/* REMINDER: Remove test code */}
         </nav>
       </section>
       <section className="deliverables">
