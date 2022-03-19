92425f5da277e6b6a96f24df09a9e3017cc13c72
Neale Taylor <nealeptaylor@gmail.com>
2022-03-18 21:40:37 -0400

    add onClick effect to deliverables

--- client/src/components/DeliverableListItem.js
+++ client/src/components/DeliverableListItem.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 export default function DeliverableListItem(props) {
   return (
-    <li className="deliverable_list_item">
+    <li className="deliverable_list_item" onClick={() => {
+      props.setProject(props.name)
+      console.log("Set project: ", props.name)
+      }
+    }>
       <span className="deliverable_name">{props.name}</span>
       <span className="deliverable_description">{props.description}</span>
     </li>
