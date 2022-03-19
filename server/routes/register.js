118efecbca351b1b6456f07bd7e67b29fddf71e3
Vivian Zhang <vvynz12@gmail.com>
2022-03-19 16:54:09 -0400

    Minor updates

---
+++ server/routes/register.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+module.exports = (db) => {
+  // all routes will go here
+  router.get('/', (req, res) => {
+    const command = "SELECT * FROM users";
+    db.query(command).then(data => {
+      return res.json(data.rows);
+    });
+  });
+
+  // PUT /users
+  router.put('/users', (req, res) => {
+    const { name, email, password, team_id } = req.body;
+    const values = [name, email, password, team_id];
+    const command = `
+    INSERT INTO users (name, email, password, team_id)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;
+    `;
+    // return db.query(command, values)
+    //   .then(data => res.send(data.rows[0]))
+    //   .catch(err => console.log(err));
+    console.log("PUT/users");
+  })
+
+  return router;
+};
