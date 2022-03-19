de4f41bd6412c6474c484bc3fbd0e5c194c2a56d
Vivian Zhang <vvynz12@gmail.com>
2022-03-18 23:44:50 -0400

    Create handleSubmit & handleCHange funcs

--- client/src/routes/register.js
+++ client/src/routes/register.js
@@ -3,22 +3,54 @@ import axios from 'axios';
 import useAppData from '../hooks/useAppData';
 
 export default function Register() {
+
+  const {
+    state,
+    setUser
+  } = useAppData();
+
+  state.user = {
+    name: '',
+    email: '',
+    password: ''
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const { name, email, password } = state.user;
+
+    alert(`name: ${name}, email: ${email}, password: ${password}`);
+  }
+
+  function handleChange(event) {
+    event.preventDefault();
+    const value = event.target.value;
+
+    setUser({
+      [event.target.user]: value
+    });
+  }
+
+  // console.log("TEST", setUser);
+
   return (
     <main>
       <h1>Register Page</h1>
       <div>
-        <form>
+        <form onSubmit={handleSubmit} onChange={handleChange}>
           <label>Name:
-          <input type="text" />
+          <input type="text" user='name' value={state.user.name} />
           </label>
           <br />
           <label>Email:
-            <input type="text" />
+            <input type="text" user='email' value={state.user.email} />
           </label>
           <br />
           <label>Password:
-            <input type="text" />
+            <input type="text" user='password' value={state.user.password} />
           </label>
+          <br />
+          <button type="submit">Register</button>
         </form>
       </div>
     </main >
