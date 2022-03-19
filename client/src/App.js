8c877135b93408220d2ab645b435fca274d5a576
Neale Taylor <nealeptaylor@gmail.com>
2022-03-19 16:54:27 -0400

    tinkering with tasks display

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,6 @@ function App() {
   //   return (user.name)
   // })
 
-
   const getDeliverablesForProject = function(state, project) {
     let result = [];
     let projects = state.projects;
@@ -29,7 +28,6 @@ function App() {
 
     for (const selectedProject of projects) {
       if (selectedProject.name === project) {
-        // console.log(selectedProject)
         for (const selectedDelivs of deliverables) {
           if (selectedDelivs.project_id === selectedProject.id) {
             console.log("Deliverable: ", selectedDelivs)
@@ -44,14 +42,15 @@ function App() {
   const getTasksForDeliverable = function(state, project, deliverable) {
     let result = [];
     let projects = state.projects;
-    let deliverables = state.deliverables;
+    let deliverables = state.deliverables
     let tasks = state.tasks
-
+    // This can be compressed a lot by calling getDeliverablesForProject, I just hadn't done it yet for testing purposes
     for (const selectedProject of projects) {
       if (selectedProject.name === project) {
         for (const selectedDelivs of deliverables) {
           if (selectedDelivs.project_id === selectedProject.id) {
             for (const selectedTasks of tasks) {
+              // Need the second half of this if statement to get the selected deliverable rather than selectedDelivs(which returns all delivs for given project)(I kind of messed up on the naming convention here)
               if (selectedTasks.deliverable_id === selectedDelivs.id) {
                 console.log("Task: ", selectedTasks)
                 result.push(selectedTasks)
@@ -86,9 +85,9 @@ function App() {
               value={state.deliverable}
               onChange={setDeliverable}
             />
-            {/* Need to hide and reveal when clicked--figure it out */}
+            {/* Need to hide and reveal when clicked */}
             <TaskList 
-              tasks={getTasksForDeliverable(state, state.project, state.deliverable)}
+              tasks={getTasksForDeliverable(state, state.project, state.deliverables)}
             />
             {/* ------------------ */}
           </nav>
