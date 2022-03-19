d1142ce09d6d4900bc3fe1b3e58651b3aed58440
mrjohnming <john.ngai@hotmail.com>
2022-03-19 14:44:02 -0400

    Uninstall body-parser

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 // const cors = require('cors'); // cors require
@@ -18,10 +17,8 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(cors());
