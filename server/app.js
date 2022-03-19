69764bd22e14077b3a395d768aa17fd347286e33
mrjohnming <john.ngai@hotmail.com>
2022-03-19 14:29:36 -0400

    Add body-parser

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors'); // cors require
+// const cors = require('cors'); // cors require
 
 const db = require('./configs/db.config');
 
@@ -17,11 +18,13 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+// app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/projects', projectsRouter(db));
