f928ffb5fc5089a5e135fbb2f7951d984e7bce43
mrjohnming <john.ngai@hotmail.com>
2022-03-18 20:29:47 -0400

    Add state

--- client/src/routes/project_new.js
+++ client/src/routes/project_new.js
@@ -1,4 +1,8 @@
+import useAppData from '../hooks/useAppData';
+
 export default function NewProject() {
+  const { state } = useAppData();
+  
   return (
     <main>
       <h1>Create a New Project</h1>
