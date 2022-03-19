3fea3eeb9616341b6749d7c14152abebef8651c6
Vivian Zhang <vvynz12@gmail.com>
2022-03-19 15:51:32 -0400

    Create PUT sql query

--- server/routes/register.jsx
+++ server/routes/register.jsx
@@ -11,7 +11,15 @@ module.exports = (db) => {
 
   // PUT /users/:id
   router.put('/users/:id', (req, res) => {
-
+    const { name, email, password, team_id } = req.body;
+    const values = [name, email, password, team_id];
+    const command = `
+    INSERT INTO users (name, email, password, team_id)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;
+    `;
+    return db.query(command, values)
+      .then(data => res.send(data.rows[0]));
   })
 
   return router;
