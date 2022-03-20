61b26a663359ccdaa21410a2d4781696692b22db
mrjohnming <john.ngai@hotmail.com>
2022-03-20 15:17:12 -0400

    Create function - deleteProject

--- client/src/hooks/useAppData.js
+++ client/src/hooks/useAppData.js
@@ -43,6 +43,25 @@ export default function useAppData() {
   const setProject = project => setState({ ...state, project });
   appData.setProject = setProject;
 
+  // Delete the currently selected project id.
+  const deleteProject = (state, project_id) => {
+    // Declare a new projects array to hold the updated projects data.
+    const projects = [];
+  
+    // Loop through each project from state,
+    for (const project of state.projects) {
+      // If the project's id is not equal to the selected project id,
+      if (project.id !== project_id) {
+        // Add the project to the projects array.
+        projects.push([project]);
+      }
+    }
+    // console.log('project_id =', project_id); // Remove test code.
+
+    
+  }
+  appData.deleteProject = deleteProject;
+
   // Set the currently selected deliverable id.
   const setDeliverable = deliverable => setState({ ...state, deliverable });
   appData.setDeliverable = setDeliverable;
