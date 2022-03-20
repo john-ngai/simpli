f78c25551f723ebae2d9f2485a9a04500a84e16e
mrjohnming <john.ngai@hotmail.com>
2022-03-20 00:38:12 -0400

    Refactor codebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
 // Dependencies
-import { React, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-
+import React from 'react';
 // Stylesheets
 import './App.css';
-
 // Hooks
 import useAppData from './hooks/useAppData';
 import useVisualMode from './hooks/useVisualMode';
-
 // Components
-import ProjectList from './components/ProjectList';
 import NavBar from './components/NavBar';
+import ProjectList from './components/ProjectList';
 import DeliverableList from './components/DeliverableList';
 import TaskList from './components/TaskList';
-
 // Modes
 const DELIVERABLES = 'DELIVERABLES';
 const TASKS = 'TASKS';
-
 
 export default function App() {
   const { state, setProject, setDeliverable, getDeliverables, getTasks } = useAppData();
@@ -28,60 +21,9 @@ export default function App() {
   const deliverables = getDeliverables(state, state.project);
   const tasks = getTasks(state, state.deliverable);
 
-  /* 
-  Example task:
-    {
-      id: 8,
-      name: 'Setup React',
-      description: '',
-      priority: false,
-      status: 'not started',
-      deliverable_id: 4
-    }  
-  */
-
-
-
-
-
-
-
-
-  const getTasksForDeliverable = function (state, project, deliverable) {
-    let result = [];
-    let projects = state.projects;
-    let deliverables = state.deliverables
-    let tasks = state.tasks
-    // This can be compressed a lot by calling getDeliverablesForProject, I just hadn't done it yet for testing purposes
-    for (const selectedProject of projects) {
-      if (selectedProject.name === project) {
-        for (const selectedDelivs of deliverables) {
-          if (selectedDelivs.project_id === selectedProject.id) {
-            for (const selectedTasks of tasks) {
-              // Need the second half of this if statement to get the selected deliverable rather than selectedDelivs(which returns all delivs for given project)(I kind of messed up on the naming convention here)
-              if (selectedTasks.deliverable_id === selectedDelivs.id) {
-                console.log("Task: ", selectedTasks)
-                result.push(selectedTasks)
-              }
-            }
-          }
-        }
-      }
-    }
-    return result
-  }
-
-
-
-
-
   return (
     <div>
       <NavBar users={state.users} />
-
-      {/* <button onClick={() => transition(DELIVERABLES)}>Deliverables</button> */}
-      {/* <button onClick={() => transition(TASKS)}>Tasks</button> */}
-      
       <main className="layout">
         <section className="projects">
           <nav>
