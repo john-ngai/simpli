dc8ecabbcd98f50d6a250678f3ac050ff113ace9
mrjohnming <john.ngai@hotmail.com>
2022-03-19 23:20:00 -0400

    Change setProject param to props.id

--- client/src/components/ProjectListItem.js
+++ client/src/components/ProjectListItem.js
@@ -3,8 +3,7 @@ import React from "react";
 export default function ProjectListItem(props) {
   return (
     <li className="project_list_item" onClick={() => {
-    props.setProject(props.name)
-    console.log("Set project: ", props.name)
+    props.setProject(props.id)
     }
     }>
       <span className="project_name">{props.name}</span>
