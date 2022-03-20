3c174143dd1736d20ec324a5b6d26413baf6fda5
mrjohnming <john.ngai@hotmail.com>
2022-03-19 22:57:21 -0400

    Refactor codebase

--- client/src/components/DeliverableList.js
+++ client/src/components/DeliverableList.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import DeliverableListItem from './DeliverableListItem';
 
+// Container for each DeliverableListItem.
 export default function DeliverableList(props) {
-  const deliverableInfo = props.deliverables.map(deliverable => {
-    return (
-      <DeliverableListItem 
-        key={deliverable.id}
-        name={deliverable.name}
-        description={deliverable.description}
-        selected={deliverable.name === props.value}
-        setDeliverable={props.onChange}
-      />
-    )
-  })
+  const listItem = props.deliverables.map(deliverable =>
+    <DeliverableListItem
+      key={deliverable.id}
+      name={deliverable.name}
+      description={deliverable.description}
+      selected={deliverable.name === props.value}
+      setDeliverable={props.onChange}
+    />
+  );
 
   return (
     <ul className="deliverable_item_list">
-      {deliverableInfo}
+      { listItem }
     </ul>
-  )
+  );
 }
