e32943d8086c1092f42c8716ad11610258956e50
vagrant <vvynz12@gmail.com>
2022-03-20 16:37:46 +0000

    Implement SQL query for login route

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,11 +3,13 @@ const router = require('express').Router();
 module.exports = (db) => {
   // all routes will go here
   router.get('/', (req, res) => {
-    const { email, password } = req.body;
-    const command = "SELECT * FROM users";
-    db.query(command).then(data => {
-      return res.json(data.rows);
-    });
+    // WILL NEED TO UPDATE WITH COOKIES LATER 
+    const { email } = req.body;
+    const values = [email];
+    const command = "SELECT * FROM users WHERE email = $1";
+    db.query(command, values)
+      .then(data => res.send(data.rows[0]))
+      .catch(err => console.log(err));
   });
   return router;
 };
