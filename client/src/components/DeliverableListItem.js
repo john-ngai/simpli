5b96933970347350b01b3c85346c5d34b5eec28c
mrjohnming <john.ngai@hotmail.com>
2022-03-20 00:01:36 -0400

    Change setDeliverable param to props.id

--- client/src/components/DeliverableListItem.js
+++ client/src/components/DeliverableListItem.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 export default function DeliverableListItem(props) {
   return (
-    <li className="deliverable_list_item" onClick={() => {
-      props.setDeliverable(props.name)
-      console.log("Set Deliverable: ", props.name)
-      }
-    }>
+    <li className="deliverable_list_item"
+      onClick={() => props.setDeliverable(props.id)}
+    >
       <span className="deliverable_name">{props.name}</span>
       <span className="deliverable_description">{props.description}</span>
     </li>
