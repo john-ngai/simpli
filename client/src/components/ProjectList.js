dd36b0b7a23f99e06f891cfd9f6959c1d223b91f
mrjohnming <john.ngai@hotmail.com>
2022-03-20 16:37:26 -0400

    Refactor codebase

--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -2,25 +2,25 @@ import React from "react";
 import ProjectListItem from "./ProjectListItem";
 
 export default function ProjectList(props) {
-  const projectInfo = props.projects.map(project => {
-    return (
-      <ProjectListItem 
-        key={project.id}
-        id={project.id}
-        name={project.name}
-        description={project.description}
-        selected={project.id === props.value}
-        setProject={() => {
-          props.onChange(project.id);
-          props.transition('DELIVERABLES');
-        }}
-      />
-    )
-  })
+  const listItem = props.projects.map(project =>
+    <ProjectListItem
+      key={project.id}
+      name={project.name}
+      description={project.description}
+      selected={project.id === props.value}
+      setProject={() => {
+        props.onChange(project.id);
+        props.transition('DELIVERABLES');
+      }}
+      deleteProject={() => {
+        props.deleteProject(props.value);
+      }}
+    />
+  );
 
   return (
     <ul className="project_item_list">
-      {projectInfo}
+      {listItem}
     </ul>
-  )
+  );
 }
