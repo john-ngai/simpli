3387bdcca0865b150eaf54aaaefe9fa581c8d409
mrjohnming <john.ngai@hotmail.com>
2022-03-19 21:54:03 -0400

    Create temp components

---
+++ client/src/components/TempDeliverables.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function TempDeliverable() {
+  return (
+    <nav>
+      <li className="deliverable_list_item">
+        <span className="deliverable_name">Deliverable Name</span>
+        <span className="deliverable_description">Deliverable Description</span>
+      </li>
+      <li className="deliverable_list_item">
+        <span className="deliverable_name">Deliverable Name</span>
+        <span className="deliverable_description">Deliverable Description</span>
+      </li>
+      <li className="deliverable_list_item">
+        <span className="deliverable_name">Deliverable Name</span>
+        <span className="deliverable_description">Deliverable Description</span>
+      </li>
+    </nav>
+  );
+}
---
+++ client/src/components/TempTasks.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function TempTasks() {
+  return (
+    <nav>
+      <h2>Deliverable Name</h2>
+      <li className="deliverable_list_item">
+        <span className="deliverable_name">Task Name</span>
+        <span className="deliverable_description">Task Description</span>
+      </li>
+      <li className="deliverable_list_item">
+        <span className="deliverable_name">Task Name</span>
+        <span className="deliverable_description">Task Description</span>
+      </li>
+      <li className="deliverable_list_item">
+        <span className="deliverable_name">Task Name</span>
+        <span className="deliverable_description">Task Description</span>
+      </li>
+      <li className="deliverable_list_item">
+        <span className="deliverable_name">Task Name</span>
+        <span className="deliverable_description">Task Description</span>
+      </li>
+    </nav>
+  );
+}
