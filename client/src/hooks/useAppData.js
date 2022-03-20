48dafe7a8d278cd8b37db2d3f61e4281a7235eb7
mrjohnming <john.ngai@hotmail.com>
2022-03-19 23:45:05 -0400

    Create function - getDeliverables

--- client/src/hooks/useAppData.js
+++ client/src/hooks/useAppData.js
@@ -4,6 +4,7 @@ import axios from "axios";
 export default function useAppData() {
   const appData = {};
 
+  // Default state.
   const [state, setState] = useState({
     project: null,
     projects: [],
@@ -13,10 +14,7 @@ export default function useAppData() {
     users: [],
   });
 
-  const setProject = project => setState({ ...state, project });
-  const setDeliverable = deliverable => setState({ ...state, deliverable });
-
-
+  // GET state data.
   useEffect(() => {
     Promise.all([
       axios.get('/projects'),
@@ -25,25 +23,40 @@ export default function useAppData() {
       axios.get('/teams'),
       axios.get('/users')
     ])
-      .then((all) => {
-        const [projects, deliverables, tasks, teams, users] = all;
-        setState(prev => ({
-          ...prev,
-          projects: projects.data,
-          deliverables: deliverables.data,
-          tasks: tasks.data,
-          teams: teams.data,
-          users: users.data
-        }))
-      })
+    .then((all) => {
+      const [projects, deliverables, tasks, teams, users] = all;
+      setState(prev => ({
+        ...prev,
+        projects: projects.data,
+        deliverables: deliverables.data,
+        tasks: tasks.data,
+        teams: teams.data,
+        users: users.data
+      }))
+    })
   }, [])
-
   appData.state = state;
+  
+  const setProject = project => setState({ ...state, project });
+  appData.setProject = setProject;
+  const setDeliverable = deliverable => setState({ ...state, deliverable });
+  appData.setDeliverable = setDeliverable;
+  
+  // Return an array of deliverables matching the selected project id.
+  const getDeliverables = (state, project_id) => {
+    const allDeliverables = state.deliverables;
+    const selectedDeliverables = [];
+    // Loop through each deliverable from state,
+    for (const deliverable of allDeliverables) {
+      // If the deliverable's project id matches the current project_id,
+      if (deliverable.project_id === project_id) {
+        // Add the deliverable to the selectedDeliverables array.
+        selectedDeliverables.push(deliverable);
+      }
+    }
+    return selectedDeliverables;
+  }
+  appData.getDeliverables = getDeliverables;
 
-  // return appData
-  return {
-    state, 
-    setProject,
-    setDeliverable
-  };
+  return appData;
 }
