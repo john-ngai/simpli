60beb1f71ce17da8c692e63bb8820bf1634b4581
mrjohnming <john.ngai@hotmail.com>
2022-03-21 19:21:24 -0400

    Update styling

--- client/src/components/DeliverableListItem.js
+++ client/src/components/DeliverableListItem.js
@@ -1,3 +1,8 @@
+import './DeliverableListItem.scss';
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 import React from 'react';
 
 export default function DeliverableListItem(props) {
@@ -8,9 +13,16 @@ export default function DeliverableListItem(props) {
         props.transition('TASKS');
       }}
     >
-      <span className="deliverable_name">{props.name}</span>
+      <div id="deliverable_list_item_header">
+        <span className="deliverable_name">{props.name}</span>
+        <PriorityHighIcon id="important_deliverable" className="mui_icons" />
+      </div>
       <span className="deliverable_description">{props.description}</span>
-      <aside>Tasks remaining: {props.count}</aside>
+      <span className="deliverable_tasks">{props.count} Tasks Remaining</span>
+      <span className="deliverable_updates">
+        <EditIcon id="edit_deliverable" className="mui_icons" />
+        <DeleteIcon id="delete_deliverable" className="mui_icons" />
+      </span>
     </li>
   )
 }
