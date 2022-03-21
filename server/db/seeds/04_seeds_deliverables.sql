1cc0bdd714e196a19d1a6ec296ffaf51cfb323d8
vagrant <vvynz12@gmail.com>
2022-03-21 19:42:47 +0000

    Update database

--- server/db/seeds/04_seeds_deliverables.sql
+++ server/db/seeds/04_seeds_deliverables.sql
@@ -1,8 +1,8 @@
-INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Clean master bedroom', '', false, 'not started', 1);
-INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Clean other bedrooms', 'Start with main floor', false, 'not started', 1);
+INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Clean master bedroom', '', FALSE, 'not started', 1);
+INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Clean other bedrooms', 'Start with main floor', FALSE, 'not started', 1);
 
-INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Create a plan', 'User stories, routes, stacks, etc', false, 'not started', 2);
-INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Setup project folder', 'Front-end, back-end, & database', false, 'not started', 2);
+INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Create a plan', 'User stories, routes, stacks, etc', FALSE, 'not started', 2);
+INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Setup project folder', 'Front-end, back-end, & database', FALSE, 'not started', 2);
 
-INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Research best courses', 'YouTube, Udemy, Books, etc.', false, 'not started', 3);
-INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Create a self-learning schedule', '', false, 'not started', 3);
+INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Research best courses', 'YouTube, Udemy, Books, etc.', FALSE, 'not started', 3);
+INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Create a self-learning schedule', '', FALSE, 'not started', 3);
