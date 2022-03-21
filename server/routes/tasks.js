917b60fa2f9fea36d89f32bfbc32657602a1ec3f
Vivian Zhang <vvynz12@gmail.com>
2022-03-20 21:52:59 -0400

    Create SQL query for PUT tasks

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -7,6 +7,20 @@ module.exports = (db) => {
     db.query(command).then(data => {
       return res.json(data.rows);
     });
+
+    router.put('/:id', (req, res) => {
+      const taskID = req.params.id;
+      const { priority } = req.body;
+      const values = [taskID, priority];
+      const command = `
+      UPDATE tasks
+      SET priority = $2
+      WHERE id = $1;
+      `;
+      return db.query(command, values)
+        .then(data => res.send(data.rows[0]))
+        .catch(err => console.log(err));
+    })
   });
   return router;
 };
