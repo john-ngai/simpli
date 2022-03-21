33f492439ca3fa9bbc0f7bf4e6e19e741fc8c907
mrjohnming <john.ngai@hotmail.com>
2022-03-21 19:11:50 -0400

    Update styling

--- client/src/components/DeliverableList.js
+++ client/src/components/DeliverableList.js
@@ -1,6 +1,8 @@
 import { React } from 'react';
 import DeliverableListItem from './DeliverableListItem';
 import NewDeliverable from './NewDeliverable';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import './DeliverableList.scss';
 
 
 // Container for each DeliverableListItem.
@@ -18,20 +20,23 @@ export default function DeliverableList(props) {
   );
 
   return (
-    <div>
-      <button className="newDeliverableButton" 
-      onClick={props.showDelivForm}
-      >New Deliverable
-      </button>
+    <section id="deliverables_list">
+      <div id="project_details">
+        <span id="project_name">Project Name</span>
+        <span id="project_description">Project Description</span>
+        <span id="project_stats">3 of 5 (60%) Deliverables Completed</span>
+        <AddCircleIcon id="new_deliverable" className="mui_icons"
+          onClick={props.showDelivForm}
+        />
+      </div>
 
       {props.showFormBoolean &&
         <NewDeliverable 
           project={props.project}
-        />}
+        />
+      }
 
-      <ul className="deliverable_item_list">
-        { listItem }
-      </ul>
-    </div>
+      { listItem }
+    </section>
   );
 }
