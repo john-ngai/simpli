7ede496623a36477f8f0b0cafecfbe2f3d19ebd1
Neale Taylor <nealeptaylor@gmail.com>
2022-03-21 12:16:00 -0400

    add button and new deliverable form to show up when deliverable is clicked

--- client/src/components/DeliverableList.js
+++ client/src/components/DeliverableList.js
@@ -7,7 +7,7 @@ import NewDeliverable from './NewDeliverable';
 
 // Container for each DeliverableListItem.
 export default function DeliverableList(props) {
-  const { state, showForm } = useAppData
+  const { state, showDelivForm } = useAppData
   const { transition } = useVisualMode(null);
 
   const listItem = props.deliverables.map(deliverable =>
@@ -22,27 +22,18 @@ export default function DeliverableList(props) {
     />
   );
 
-  // const newForm = function(
-  //   state.showForm &&
-  //   <NewDeliverable 
-  //     project={props.project}
-  //   />)
-
-  // {state.showForm &&
-  //   <NewDeliverable 
-  //     project={state.project}
-  //   />}
-
   return (
     <div>
-      {/* <button className="newDeliverableButton" onClick={() => {
-        {props.showForm}; transition('DELIVERABLES')}}>New Deliverable
+      <button className="newDeliverableButton" 
+      onClick={props.showDelivForm}
+      >New Deliverable
       </button>
 
-      {props.showForm &&
+      {props.showFormBoolean &&
         <NewDeliverable 
           project={props.project}
-        />} */}
+        />}
+
       <ul className="deliverable_item_list">
         { listItem }
       </ul>
