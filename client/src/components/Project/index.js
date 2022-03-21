eb605ab89539ff34e81a06bd4da64c5017cf7321
mrjohnming <john.ngai@hotmail.com>
2022-03-20 22:02:55 -0400

    Create component - Project

---
+++ client/src/components/Project/index.js
@@ -0,0 +1,22 @@
+export default function Project() {
+  return (
+    <div>
+      <h1>Create a New Project</h1>
+      <form>
+        <label>Project Name:</label>
+        <br />
+        <input />
+
+        <br /><br />        
+
+        <label>Project Description:</label>
+        <br />
+        <input />
+
+        <br /><br />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
