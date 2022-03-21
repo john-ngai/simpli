851b590f415841de2b67c5463b01fae6bfc30583
Vivian Zhang <vvynz12@gmail.com>
2022-03-20 23:25:42 -0400

    Create a props.selected view on the component

--- client/src/components/DeliverableListItem.js
+++ client/src/components/DeliverableListItem.js
@@ -9,13 +9,21 @@ export default function DeliverableListItem(props) {
   const deliverableClass = classNames("deliverable_list_item", {
     "deliverable_list_item--selected": props.selected
   })
+
+  const priorityIcon = classNames("PriorityHighIcon", {
+    "PriorityHighIcon--selected"
+  })
+
   return (
-    <li className={deliverableClass}
-      onClick={props.setDeliverable}>
-      <PriorityHighIcon onDoubleClick={() => onToggle(id)} />
+    <li className={deliverableClass} onClick={props.setDeliverable}>
       <span className="deliverable_name">{props.name}</span>
       <span className="deliverable_description">{props.description}</span>
       <aside>Tasks remaining: {props.count}</aside>
+      {props.selected &&
+        <span className="deliverable_edit">
+          <PriorityHighIcon className="PriorityHighIcon" onDoubleClick={() => onToggle(id)} />
+        </span>
+      }
     </li >
   )
 }
