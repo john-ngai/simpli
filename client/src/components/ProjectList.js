aad332be0e35f0a6e92b55ad0ee3fa93cfaf7130
Neale Taylor <nealeptaylor@gmail.com>
2022-03-21 18:02:47 -0400

    add percent complete prop

--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -1,25 +1,28 @@
 import React from "react";
 import ProjectListItem from "./ProjectListItem";
 import { Link } from 'react-router-dom';
+import useAppData from "../hooks/useAppData";
 
 export default function ProjectList(props) {
+  const { state, percentComplete } = useAppData();
   const listItem = props.projects.map(project =>
     <ProjectListItem
-      key={project.id}
-      name={project.name}
-      description={project.description}
-      count={project.count}
-      selected={project.id === props.value}
-      setProject={() => {
-        props.onChange(project.id);
-        props.transition('DELIVERABLES');
-      }}
-      deleteProject={() => {
-        props.deleteProject(props.value);
-      }}
+    key={project.id}
+    name={project.name}
+    description={project.description}
+    count={project.count}
+    selected={project.id === props.value}
+    percentComplete={percentComplete(state, project.id)}
+    
+    setProject={() => {
+      props.onChange(project.id);
+      props.transition('DELIVERABLES');
+    }}
+    deleteProject={() => {
+      props.deleteProject(props.value);
+    }}
     />
   );
-
   return (
     <div>
       <span className="nav-tab">
