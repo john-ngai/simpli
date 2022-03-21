b0c1545de881c287fc77925f66641b377e43a773
mrjohnming <john.ngai@hotmail.com>
2022-03-21 14:45:44 -0400

    Create copy of App.js

---
+++ client/src/DevApp.jsx
@@ -0,0 +1,80 @@
+// Dependencies
+import React from 'react';
+// Stylesheets
+import './App.css';
+// Hooks
+import useAppData from './hooks/useAppData';
+import useVisualMode from './hooks/useVisualMode';
+// Components
+import NavBar from './components/NavBar';
+import ProjectList from './components/ProjectList';
+import DeliverableList from './components/DeliverableList';
+import TaskList from './components/TaskList';
+import Project from './components/Project';
+// Modes
+const DELIVERABLES = 'DELIVERABLES';
+const PROJECTS = 'PROJECTS';
+const TASKS = 'TASKS';
+const SAVING = 'SAVING';
+const NEW_PROJECT = 'NEW_PROJECT';
+const EDIT_PROJECT = 'EDIT_PROJECT';
+const NEW_DELIVERABLE = 'NEW_DELIVERABLE';
+const NEW_TASK = 'NEW_TASK';
+
+export default function App() {
+  const {
+    state,
+    saveProject,
+    setProject, setDeliverable,
+    getDeliverables, getTasks, showDelivForm, showTaskForm, deleteProject
+  } = useAppData();
+
+  const { mode, transition, back } = useVisualMode(null);
+
+  const deliverables = getDeliverables(state, state.project);
+  const tasks = getTasks(state, state.deliverable);
+
+  return (
+    <div>
+      <NavBar users={state.users} />
+      
+      <button onClick={() => transition(NEW_PROJECT)}>NEW_PROJECT</button>
+
+      <main className="layout">
+        <section className="projects">
+          <nav>
+            <ProjectList
+              projects={Object.values(state.projects)}
+              value={state.project}
+              onChange={setProject}
+              transition={transition}
+              deleteProject={deleteProject}
+            />
+          </nav>
+        </section>
+        <section className="deliverables">
+
+          {mode === NEW_PROJECT && <Project
+            saveProject={saveProject}
+            back={back}
+          />}
+
+          {mode === DELIVERABLES && <DeliverableList
+            deliverables={deliverables}
+            onChange={setDeliverable}
+            onClick={transition}
+            project={state.project}
+            showFormBoolean={state.showDelivForm}
+            showDelivForm={showDelivForm}
+          />}
+          {mode === TASKS && <TaskList
+            tasks={tasks}
+            deliverable={state.deliverable}
+            showFormBoolean={state.showTaskForm}
+            showTaskForm={showTaskForm}
+          />}
+        </section>
+      </main>
+    </div>
+  );
+}
