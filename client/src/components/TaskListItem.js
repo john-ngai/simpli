653c4aa1001b361202a7e8a7f70b7d77d3d06a33
vagrant <vvynz12@gmail.com>
2022-03-21 14:39:07 +0000

    Update task class name depending on if selected or not

--- client/src/components/TaskListItem.js
+++ client/src/components/TaskListItem.js
@@ -4,12 +4,15 @@ import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 
 export default function TaskListItem(props) {
   const { task, onToggle } = props;
+  const taskClass = classNames("PriorityHighIcon", {
+    "PriorityHighIcon--selected": props.selected
+  })
 
   return (
     <li className="task_list_item">
       <span className="task_name">{props.name}</span>
       <span className="task_description">{props.description}</span>
-      <PriorityHighIcon className="PriorityHighIcon priority" onClick={() => onToggle(task.id)} />
+      <PriorityHighIcon className={taskClass} onClick={() => onToggle(task.id)} />
     </li>
   )
 }
