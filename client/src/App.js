eed3582a488b8a0b86ed69e721aa3a9460bc4e96
Neale Taylor <nealeptaylor@gmail.com>
2022-03-21 18:03:35 -0400

    remove test code

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,6 @@ export default function App() {
 
   const deliverables = getDeliverables(state, state.project);
   const tasks = getTasks(state, state.deliverable);
-// console.log(percentComplete(state, state.project))
   return (
     <div>
       <NavBar users={state.users} />
