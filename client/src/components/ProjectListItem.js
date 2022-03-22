b45a62fcdfb929c2eb8df7d4a24744302d8e3315
mrjohnming <john.ngai@hotmail.com>
2022-03-21 20:30:29 -0400

    Remove commented code

--- client/src/components/ProjectListItem.js
+++ client/src/components/ProjectListItem.js
@@ -25,21 +25,5 @@ export default function ProjectListItem(props) {
         </span>
       }
     </li>
-
-    // <li className={projectClass} onClick={props.setProject}>
-    //   <span className="project_name">{props.name}</span>
-    //   <span className="project_description">{props.description}</span>
-    //   <aside>Deliverables: {props.count}</aside>
-    //   {props.selected &&
-    //     <span className="project_edit">
-    //       <EditIcon className="mui_icons EditIcon"
-    //         onClick={props.editProject}
-    //       />
-    //       <DeleteIcon className="mui_icons DeleteIcon"
-    //         onClick={props.deleteProject}
-    //       />
-    //     </span>
-    //   }
-    // </li>
   );
 };  
