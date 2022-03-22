d2fbac2d81a674a9e2e34f941d2eb9d0158cf0ce
mrjohnming <john.ngai@hotmail.com>
2022-03-21 20:01:13 -0400

    Add styling

--- client/src/components/TaskListItem.js
+++ client/src/components/TaskListItem.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function TaskListItem(props) {
   return (
-    <li className="task_list_item">
-      <span className="task_name">{props.name}</span>
-      <span className="task_description">{props.description}</span>
+    <li className="task_list_item"
+      onClick=""
+    >
+      <CheckCircleOutlineIcon id="completed_task" className="mui_icons" />
+      <div id="task_list_item_container">
+        <div id="task_list_item_header">
+          <span className="task_name">{props.name}</span>
+          <PriorityHighIcon id="important_task" className="mui_icons" />
+        </div>
+        <span className="task_description">{props.description}</span>
+        <span className="task_updates">
+          <EditIcon id="edit_task" className="mui_icons" />
+          <DeleteIcon id="delete_task" className="mui_icons" />
+        </span>
+      </div>
     </li>
-  )
+  );
 }
