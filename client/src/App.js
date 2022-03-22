dee5e00408ad87871bbc5ef45e3ea26a50b287ea
mrjohnming <john.ngai@hotmail.com>
2022-03-22 12:24:17 -0400

    Remove test code

--- client/src/App.js
+++ client/src/App.js
@@ -37,8 +37,6 @@ export default function App() {
   const deliverables = getDeliverables(state, state.project);
   const tasks = getTasks(state, state.deliverable);
 
-  console.log(selectedProject);
-
   return (
     <div id="container">
       <NavBar users={state.users} />
