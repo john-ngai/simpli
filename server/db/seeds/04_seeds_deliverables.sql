dab818ed1f8d511ad2422a2877a579895277649c
mrjohnming <john.ngai@hotmail.com>
2022-03-21 20:41:42 -0400

    Update seeds

--- server/db/seeds/04_seeds_deliverables.sql
+++ server/db/seeds/04_seeds_deliverables.sql
@@ -1,5 +1,5 @@
 INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Clean master bedroom', '', false, 'not started', 1);
-INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Clean other bedrooms', 'Start with main floor', false, 'not started', 1);
+INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Clean other bedrooms', 'Start with main floor', false, 'completed', 1);
 
 INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Create a plan', 'User stories, routes, stacks, etc', false, 'not started', 2);
 INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Setup project folder', 'Front-end, back-end, & database', false, 'not started', 2);
