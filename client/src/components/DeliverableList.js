c9a6910d4fd29c490061ee0520e435718ba0eace
mrjohnming <john.ngai@hotmail.com>
2022-03-22 11:22:43 -0400

    Fix merge conflicts

--- client/src/components/DeliverableList.js
+++ client/src/components/DeliverableList.js
@@ -27,8 +27,10 @@ export default function DeliverableList(props) {
   return (
     <section>
       <div id="project_details">
-        <span id="project_name">{props.selectedProject.name}</span>
-        <span id="project_description">{props.selectedProject.description}</span>
+        {/* <span id="project_name">{props.selectedProject.name}</span>
+        <span id="project_description">{props.selectedProject.description}</span> */}
+        <span id="project_name">Project Name</span>
+        <span id="project_description">Project Description</span>
         <span id="project_stats">3 of 5 (60%) Deliverables Completed</span>
         <AddCircleIcon id="new_deliverable" className="mui_icons"
           onClick={props.showDelivForm}
