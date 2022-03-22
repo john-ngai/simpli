0386ff87fbf13dc94c907bc0db1b970f7f3f7eba
vagrant <vvynz12@gmail.com>
2022-03-22 21:33:26 +0000

    Minor update

--- client/src/components/TaskListItem.js
+++ client/src/components/TaskListItem.js
@@ -7,7 +7,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function TaskListItem(props) {
   const { task, onToggle, onClick } = props;
-  console.log("PROPS SELECTED:", props.selected);
+  // console.log("PROPS SELECTED:", props.selected);
   // const taskClass = classNames("task_list_item", {
   //   "task_list_item--selected": props.selected
   // })
