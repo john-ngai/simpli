adb26828aa6eea9fe28a9376636c1201d626f15c
vagrant <vvynz12@gmail.com>
2022-03-22 21:34:04 +0000

    Remove unused code

--- client/src/components/TaskListItem.js
+++ client/src/components/TaskListItem.js
@@ -15,7 +15,6 @@ export default function TaskListItem(props) {
   return (
     <li className="task_list_item">
       <CheckCircleOutlineIcon id={props.complete ? "completed_task" : "incomplete_task"} className="mui_icons" onClick={() => onClick(task.id)} />
-      {/* <CheckCircleOutlineIcon id="complete_task" className="mui_icons" /> */}
       <div id="task_list_item_container">
         <div id="task_list_item_header">
           <span className="task_name">{props.name}</span>
