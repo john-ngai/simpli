cebdda27a9e0044feb671117a50370a0e6e582db
mrjohnming <john.ngai@hotmail.com>
2022-03-21 20:28:29 -0400

    Fix bug

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import DevApp from './DevApp'; // Remove test code or change component name to 'App'.
 import Register from './routes/register';
 import Login from './routes/login';
 import NewProject from './routes/project_new';
