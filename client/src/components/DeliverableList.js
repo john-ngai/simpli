4967b226bef36d958dedbf2778bb89b7a88fcfd6
Neale Taylor <nealeptaylor@gmail.com>
2022-03-22 15:48:34 -0400

    add prop for savedeliverable

--- client/src/components/DeliverableList.js
+++ client/src/components/DeliverableList.js
@@ -4,11 +4,14 @@ import DeliverableListItem from './DeliverableListItem';
 import NewDeliverable from './NewDeliverable';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import './DeliverableList.scss';
-
+import useVisualMode from '../hooks/useVisualMode';
+const NEW_DELIVERABLE = 'NEW_DELIVERABLE';
 
 // Container for each DeliverableListItem.
 export default function DeliverableList(props) {
-  const { state, deliverablePercentComplete } = useAppData();
+  const { state, deliverablePercentComplete, saveDeliverable } = useAppData();
+  const { mode, transition, back } = useVisualMode(null);
+
   const listItem = props.deliverables.map(deliverable =>
     <DeliverableListItem
       key={deliverable.id}
@@ -33,13 +36,19 @@ export default function DeliverableList(props) {
         <span id="project_description">Project Description</span>
         <span id="project_stats">3 of 5 (60%) Deliverables Completed</span>
         <AddCircleIcon id="new_deliverable" className="mui_icons"
-          onClick={props.showDelivForm}
+          onClick={() => {
+            // props.transition('NEW_DELIVERABLE')
+            props.showDelivForm()
+          }}
         />
       </div>
 
       {props.showFormBoolean &&
         <NewDeliverable
           project={props.project}
+          transition={transition}
+          showDelivForm={props.showDelivForm}
+          saveDeliverable={props.saveDeliverable}
         />
       }
 
