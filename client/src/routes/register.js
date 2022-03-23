280a478406bc5a2da0bef4a6fa516e41a8405e0f
mrjohnming <john.ngai@hotmail.com>
2022-03-23 15:34:12 -0400

    Add component - NavBar

--- client/src/routes/register.js
+++ client/src/routes/register.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router';
 import { FormGroup, FormControl, TextField } from '@mui/material';
 import useAppData from '../hooks/useAppData';
+import NavBar from '../components/NavBar';
 
 export default function Register() {
   const { state } = useAppData();
@@ -19,17 +20,14 @@ export default function Register() {
       setError("A name is required");
       return;
     }
-
     if (!email) {
       setError("An email is required");
       return;
     }
-
     if (!password) {
       setError("A password is required");
       return;
     }
-
     // resets setError to prev state and saves the registered user
     setError("");
     registerUser(name, email, password);
@@ -43,39 +41,33 @@ export default function Register() {
       password: password,
       team_id: 1 //temporary id assigned. Will need to update later
     }
-    console.log("New user registered!", user);
-
     axios.put('/register', user)
       .then(res => console.log('success?', res.data))
       .then(navigate('/'))
   }
 
-  function handleClick() {
-    navigate('/');
-  }
-
   return (
-    <main>
-      <button onClick={handleClick}>Back to Home</button>
-      <h1>Register Page</h1>
-      <div>
-        <section className="user_validation">{error}</section>
-        <br />
-
-        <FormGroup onSubmit={(e) => e.preventDefault()} >
-          <FormControl>
-            <TextField label="Name" type="text" value={name} placeholder="Enter your full name" onChange={(e) => setName(e.target.value)} />
-            <br />
-            <TextField label="Email" type="text" value={email} placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
-            <br />
-            <TextField label="Password" type="password" value={password} placeholder="Enter password" onChange={(e) => setPassword(e.target.value)} />
-            <br />
-            <button type="submit" onClick={validation}>Register</button>
-          </FormControl>
-        </FormGroup>
-
-      </div>
-    </main >
+    <div id="container">
+      <NavBar users={state.users} />
+      <main>
+        <h1>Registration Page</h1>
+        <div>
+          <section className="user_validation">{error}</section>
+          <br />
+          <FormGroup onSubmit={(e) => e.preventDefault()} >
+            <FormControl>
+              <TextField label="Name" type="text" value={name} placeholder="Enter your full name" onChange={(e) => setName(e.target.value)} />
+              <br />
+              <TextField label="Email" type="text" value={email} placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
+              <br />
+              <TextField label="Password" type="password" value={password} placeholder="Enter password" onChange={(e) => setPassword(e.target.value)} />
+              <br />
+              <button type="submit" onClick={validation}>Register</button>
+            </FormControl>
+          </FormGroup>
+        </div>
+      </main>
+    </div>
   );
 }
 
