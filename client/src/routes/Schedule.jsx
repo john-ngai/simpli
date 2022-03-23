73cbfb4e384138621eeb34beae210981975ca941
vagrant <vvynz12@gmail.com>
2022-03-23 16:33:12 +0000

    Create a Shedule component and import react and NavBar

---
+++ client/src/routes/Schedule.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+export default function Schedule() {
+  return (
+    <div>
+      <NavBar />
+      <main>
+
+      </main>
+    </div>
+  )
+}
