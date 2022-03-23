2240f49e73ec0af471f52f3275e3a3aabddb1ffc
vagrant <vvynz12@gmail.com>
2022-03-23 17:19:10 +0000

    Import fullCalendar timegrid plugin

--- client/src/routes/Schedule.jsx
+++ client/src/routes/Schedule.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/timegrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import NavBar from "../components/NavBar";
 
 export default function Schedule() {
   return (
     <div>
       <NavBar />
-      <main>
-        <FullCalendar plugins={[dayGridPlugin]} initialView="timeGridWeek" />
-      </main>
+      <div>
+        <FullCalendar plugins={[timeGridPlugin]} initialView="timeGridWeek" />
+      </div>
     </div>
   )
 }
