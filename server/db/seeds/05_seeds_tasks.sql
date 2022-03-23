d57f91a7d4a5dfa2e60deef7d43910f7d1939824
Neale Taylor <nealeptaylor@gmail.com>
2022-03-23 14:21:20 -0400

    change complete into status

--- server/db/seeds/05_seeds_tasks.sql
+++ server/db/seeds/05_seeds_tasks.sql
@@ -1,16 +1,16 @@
-INSERT INTO tasks (name, description, priority, complete, deliverable_id) VALUES ('Clean the bathroom', 'Use bleach', FALSE, false, 1);
+INSERT INTO tasks (name, description, priority, status, deliverable_id) VALUES ('Clean the bathroom', 'Use bleach', FALSE, false, 1);
 
-INSERT INTO tasks (name, description, priority, complete, deliverable_id) VALUES ('Clean out the closet', 'Place valuables somewhere safe', false, false, 2);
-INSERT INTO tasks (name, description, priority, complete, deliverable_id) VALUES ('Remove furniture', '', false, true, 2);
+INSERT INTO tasks (name, description, priority, status, deliverable_id) VALUES ('Clean out the closet', 'Place valuables somewhere safe', false, false, 2);
+INSERT INTO tasks (name, description, priority, status, deliverable_id) VALUES ('Remove furniture', '', false, true, 2);
 
-INSERT INTO tasks (name, description, priority, complete, deliverable_id) VALUES ('Outline user stories', '', false, false, 3);
-INSERT INTO tasks (name, description, priority, complete, deliverable_id) VALUES ('Outline routes', '', false, true, 3);
-INSERT INTO tasks (name, description, priority, complete, deliverable_id) VALUES ('Draw website mockup and ERDs', '', false, false, 3);
+INSERT INTO tasks (name, description, priority, status, deliverable_id) VALUES ('Outline user stories', '', false, false, 3);
+INSERT INTO tasks (name, description, priority, status, deliverable_id) VALUES ('Outline routes', '', false, true, 3);
+INSERT INTO tasks (name, description, priority, status, deliverable_id) VALUES ('Draw website mockup and ERDs', '', false, false, 3);
 
-INSERT INTO tasks (name, description, priority, complete, deliverable_id) VALUES ('Setup Express server', '', false, false, 4);
-INSERT INTO tasks (name, description, priority, complete, deliverable_id) VALUES ('Setup React', '', false, true, 4);
-INSERT INTO tasks (name, description, priority, complete, deliverable_id) VALUES ('Setup PostgreSQL database', '', false, false, 4);
+INSERT INTO tasks (name, description, priority, status, deliverable_id) VALUES ('Setup Express server', '', false, false, 4);
+INSERT INTO tasks (name, description, priority, status, deliverable_id) VALUES ('Setup React', '', false, true, 4);
+INSERT INTO tasks (name, description, priority, status, deliverable_id) VALUES ('Setup PostgreSQL database', '', false, false, 4);
 
-INSERT INTO tasks (name, description, priority, complete, deliverable_id) VALUES ('Create a shortlist of 3-5 courses', '', false, true, 5);
+INSERT INTO tasks (name, description, priority, status, deliverable_id) VALUES ('Create a shortlist of 3-5 courses', '', false, true, 5);
 
-INSERT INTO tasks (name, description, priority, complete, deliverable_id) VALUES ('Create a schedule using Google Sheets', '', FALSE, false, 6);
+INSERT INTO tasks (name, description, priority, status, deliverable_id) VALUES ('Create a schedule using Google Sheets', '', FALSE, false, 6);
