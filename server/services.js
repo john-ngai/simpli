917ad947cebefddf6cb2f2effe510795e4c47020
mrjohnming <john.ngai@hotmail.com>
2022-03-23 19:56:18 -0400

    Add comments

--- server/services.js
+++ server/services.js
@@ -1,9 +1,11 @@
 // Dependencies
 const bcrypt = require('bcrypt');
 
+// Return an encrypted password.
 const hashPassword = password => bcrypt.hashSync(password, 10);
 exports.hashPassword = hashPassword;
 
+// Check if an unencrypted and encrypted password are a match.
 const checkPassword = (input, hash) => {
   return bcrypt.compareSync(input, hash);
 }
