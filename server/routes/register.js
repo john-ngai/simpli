0bede16ecfda2b157b593696fa5ed980cfd90125
mrjohnming <john.ngai@hotmail.com>
2022-03-24 15:51:18 -0400

    Update route - from PUT to POST /register

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const services = require('../services');
 const database = require('../database');
-const hexNumGenerator = require('../helpers/hexNumGenerator');
 
 module.exports = (db) => {
   // GET /register
@@ -13,53 +12,43 @@ module.exports = (db) => {
   });
 
   // PUT /register
-  router.put('/', (req, res) => {
+  router.post('/', (req, res) => {
     const { name, email, password, team } = req.body;
-    let team_id = null;
-
+    let team_id;
     // If the user exists, return an error.
     database.findUserByEmail(email)
       .then(data => {
         if (data) {
-          return res.send({ error: 'registered email' });
+          res.send({ error: 'registered email' });
+          return Promise.reject();
         }
       })
-
       .then(() => database.findTeamByCode(team))
       .then(data => {
         // If the team code doesn't exist, return an error,
         if (!data) {
-          return res.send({ error: 'invalid team' });
-        // Otherwise, store the existing team id in a variable.
+          res.send({ error: 'invalid team' });
+          return Promise.reject();
+          // Otherwise, store the existing team id in a variable.
         } else {
           return data.id ? team_id = data.id : null;
         }
       })
-
-
-      // .then(() => {
-      //   const hashedPassword = services.hashPassword(password);
-      //   if (!team_id) {
-      //     // First create a new team, get the team id, then create a new user.
-      //     team = hexNumGenerator(6);
-      //     const values = [team];
-
-      //   } else {
-      //     // Create 
-      //   }
-        
-      // });
-      
-    
-    // const values = [name, email, hashedPassword, 1];
-    // const command = `
-    // INSERT INTO users (name, email, password, team_id)
-    // VALUES ($1, $2, $3, $4)
-    // RETURNING *;
-    // `;
-    // return db.query(command, values)
-    //   .then(data => res.send(data.rows[0]))
-    //   .catch(err => console.log(err));
+      // Create and return a new team if the given team id value is null.
+      .then(() => database.createTeam(team_id))
+      .then(data => team_id = data.id)
+      // Create a new user.
+      .then(() => {
+        const hashedPassword = services.hashPassword(password);
+        database.createUser(name, email, hashedPassword, team_id)
+      })
+      // Create a JSON web token and attach it to the response.
+      .then(() => {
+        const user = { name, email, team_id };
+        const accessToken = services.generateToken(user);
+        user.accessToken = accessToken;
+        res.send(user);
+      });
   })
 
   return router;
