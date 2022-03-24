4dea621dc867d041f3bc70eb83089fb1b7081be0
mrjohnming <john.ngai@hotmail.com>
2022-03-24 14:01:53 -0400

    Create SQL query helper functions

---
+++ server/database.js
@@ -0,0 +1,35 @@
+// Dependencies
+const db = require('./configs/db.config');
+const { hexNumGenerator } = require('./helpers/hexNumGenerator');
+
+// Return the user if it exists.
+const findUserByEmail = email => {
+  const values = [email];
+  const command = `
+    SELECT * FROM users
+    WHERE email = $1;
+  `;
+  return db.query(command, values)
+    .then(res => res.rows[0]);
+}
+exports.findUserByEmail = findUserByEmail;
+
+// Return the team if it exists or generate a new team code.
+const findTeamByCode = code => {
+  // Return a randomly generated team code.
+  if (code === '') {
+    return {
+      id: null,
+      code: hexNumGenerator(6)
+    }
+  }
+  // Return the team if it exists.
+  const values = [code];
+  const command = `
+    SELECT * FROM teams
+    WHERE code = $1;
+  `;
+  return db.query(command, values)
+    .then(res => res.rows[0]);
+}
+exports.findTeamByCode = findTeamByCode;
