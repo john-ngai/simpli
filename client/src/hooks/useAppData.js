17a0928bf75dd14f5baef1b1b4a0f4d66c3e3e95
mrjohnming <john.ngai@hotmail.com>
2022-03-24 17:22:35 -0400

    Add JWT authentication to GET /projects, /deliverables, /tasks

--- client/src/hooks/useAppData.js
+++ client/src/hooks/useAppData.js
@@ -20,9 +20,9 @@ export default function useAppData() {
 
   useEffect(() => {
     Promise.all([
-      axios.get('/projects/auth', { headers: authHeader() }),
-      axios.get('/deliverables/auth', { headers: authHeader() }),
-      axios.get('/tasks/auth', { headers: authHeader() })
+      axios.get('/projects', { headers: authHeader() }),
+      axios.get('/deliverables', { headers: authHeader() }),
+      axios.get('/tasks', { headers: authHeader() })
     ])
       .then(all => {
         const [projects, deliverables, tasks] = all;
