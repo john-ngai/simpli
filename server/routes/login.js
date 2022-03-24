18b203cc1256901aaf178ad9ecf2896293c1cf83
mrjohnming <john.ngai@hotmail.com>
2022-03-23 22:30:09 -0400

    Create route - POST /login

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,19 +1,37 @@
 const router = require('express').Router();
+const services = require('../services');
 
 module.exports = (db) => {
-  // all routes will go here
+
+  // POST /login
   router.post('/', (req, res) => {
     const { email, password } = req.body;
-    const values = [email, password];
-    const command = `
+    values = [email];
+    command = `
       SELECT * FROM users
-      WHERE email = 'rick.sandchez@gmail.com';
+      WHERE email = $1;
     `;
+    db.query(command, values)
+      // If the user is found,
+      .then(data => {
+        const hash = data.rows[0].password;
+        // If the passwords match,
+        if (services.checkPassword(password, hash)) {
+          const { name, email, team_id } = data.rows[0];
+          const user = { name, email, team_id };
+          const accessToken = services.generateToken(user);
+          user.accessToken = accessToken;
+          // Respond with a generated JSON web token.
+          res.send(user);
+        // If the passwords do not match,
+        } else {
+          // Respond without any data.
+          res.send();
+        }
+      })
+      // If the user is not found, respond with an error code 500.
+      .catch(() => res.status(500).send());
+  })
 
-    return db.query(command, values)
-      .then(data => res.send(data.rows[0]))
-      .catch(err => res.send(err));
-  });
-  
   return router;
 };
