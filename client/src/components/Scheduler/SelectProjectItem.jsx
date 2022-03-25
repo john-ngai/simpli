698a3966aa2cb8d7960fbf0f238b147fc0d0925c
Vivian Zhang <vvynz12@gmail.com>
2022-03-25 19:54:19 -0400

    Create a SelectProjectItem and pass in props from SelectProject component

---
+++ client/src/components/Scheduler/SelectProjectItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ListItemText from '@mui/material/ListItemText';
+
+export default function SelectProjectItem(props) {
+  return (
+    <ListItemText>
+      
+    </ListItemText>
+  );
+}
