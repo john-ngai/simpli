5761f0c6bcfea75d42cca59ee0db9821e4a4fda7
Neale Taylor <nealeptaylor@gmail.com>
2022-03-25 19:40:41 -0400

    remove commented out code

--- client/src/components/Scheduler/index.jsx
+++ client/src/components/Scheduler/index.jsx
@@ -6,93 +6,12 @@ import SelectProject from './SelectProject';
 import PopupForm from './Form';
 // Material-UI
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-// import Box from '@mui/material/Box';
-// import Modal from '@mui/material/Modal';
-// import FormControl, { useFormControl } from '@mui/material/FormControl';
-// import { TextField } from '@mui/material';
-// import TimePicker from '@mui/lab/TimePicker';
-// import LocalizationProvider from '@mui/lab/LocalizationProvider';
-// import AdapterDateFns from '@mui/lab/AdapterDateFns';
-// import MenuItem from '@mui/material/MenuItem';
-// import { Typography, Button } from '@mui/material';
 
 export default function Scheduler(props) {
   let user = null;
 
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(!open);
-
-  // const data = {
-  //   "1": {
-  //     "id":1, 
-  //     "start_time":"12pm", 
-  //     "end_time":"2pm", 
-  //     "day_id":2, 
-  //     "task": {
-  //       "id":2,
-  //       "name":"Clean out the closet",
-  //       "description":"Place valuables somewhere safe"
-  //     }
-  //   }
-  // }
-  // // To access values: 
-  // // defaultValue={data[1].task.name}
-
-  // const days = [
-  //   {
-  //     value: "Sunday",
-  //     label: "Sunday"
-  //   },
-  //   {
-  //     value: "Monday",
-  //     label: "Monday"
-  //   },
-  //   {
-  //     value: "Tuesday",
-  //     label: "Tuesday"
-  //   },
-  //   {
-  //     value: "Wednesday",
-  //     label: "Wednesday"
-  //   },
-  //   {
-  //     value: "Thursday",
-  //     label: "Thursday"
-  //   },
-  //   {
-  //     value: "Friday",
-  //     label: "Friday"
-  //   },
-  //   {
-  //     value: "Saturday",
-  //     label: "Saturday"
-  //   }
-  // ]
-
-
-  // const [value, setValue] = useState(new Date());
-  // const [day, setDay] = useState('Monday')
-
-  // const handleChange = (newValue) => {
-  //   setValue(newValue);
-  // };
-
-  // const handleDayChange = (event) => {
-  //   setDay(event.target.value);
-  // };
-
-
-  // const popupStyle = {
-  //   position: 'absolute',
-  //   top: '50%',
-  //   left: '50%',
-  //   transform: 'translate(-50%, -50%)',
-  //   width: 500,
-  //   bgcolor: 'background.paper',
-  //   border: '2px solid #000',
-  //   boxShadow: 24,
-  //   p: 4,
-  // };
 
   if (!localStorage.user) {
     return (
@@ -125,76 +44,14 @@ console.log(open)
             onClick={handleOpen}  
           />
         </aside>
-        <PopupForm 
-        open={open}
-        handleOpen={handleOpen}
-        />
-{/* 
-          <div>
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={popupStyle}>
-              <Typography id="modal-modal-title" variant="h6" component="h2">
-                Schedule a Task
-              </Typography>
-              <FormControl sx={{ width: '50ch' }}>
-                <TextField
-                  id="standard-helperText"
-                  label="Task Name"
-                  // defaultValue={data[1].task.name}
-                  helperText="Task Name"
-                  variant="standard"
-                />
-                <TextField
-                  id="standard-helperText"
-                  label="Task Description"
-                  // defaultValue={data[1].task.description}
-                  helperText="Task Description"
-                  variant="standard"
-                />
-                <div style={{display:"flex"}} >
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <TimePicker
-                  label="Start Time"
-                  value={value}
-                  onChange={handleChange}
-                  renderInput={(params) => <TextField {...params} />}
-                />
-                <TimePicker
-                  label="End Time"
-                  value={value}
-                  onChange={handleChange}
-                  renderInput={(params) => <TextField {...params} />}
-                  />
-                </LocalizationProvider>
-                </div>
-                <TextField
-                  id="standard-select-day"
-                  select
-                  label="Select"
-                  value={day}
-                  onChange={handleDayChange}
-                  helperText="Select a day"
-                  variant="standard"
-                >
-                  {days.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </FormControl>
-              <Button variant="outlined" size="small">
-                Save
-              </Button>
-            </Box>
-          </Modal>
+
+        <div>
+          <PopupForm 
+          open={open}
+          handleOpen={handleOpen}
+          />
         </div>
-         */}
+
         <section id="calendar">
           <table id="table">
             <tr>
@@ -406,10 +263,8 @@ console.log(open)
               <td class="table item"></td>
               <td class="table item last"></td>
             </tr>
-
           </table>
         </section>
-
       </main>
     </div>
     
