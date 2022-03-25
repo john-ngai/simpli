5e11ca79a1b36b0f0bcfc0db286a8fd7650a63e2
Vivian Zhang <vvynz12@gmail.com>
2022-03-25 17:49:50 -0400

    Createa a SelectDeliverable component and export it

---
+++ client/src/components/Scheduler/SelectDeliverable.jsx
@@ -0,0 +1,10 @@
+import { ListItemButton, ListItemText } from '@mui/material';
+import * as React from 'react';
+
+export default function SelectDeliverable() {
+  return (
+    <ListItemButton>
+
+    </ListItemButton>
+  );
+}
