cbd7655473d2dcf84226e82d661605c55ffbbc3f
mrjohnming <john.ngai@hotmail.com>
2022-03-25 09:03:41 -0400

    Create component - SelectProject

---
+++ client/src/components/Scheduler/SelectProject.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function SelectProject() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Select Project</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={'projectID'}>Project Name #1</MenuItem>
+          <MenuItem value={'projectID'}>Project Name #2</MenuItem>
+          <MenuItem value={'projectID'}>Project Name #3</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
