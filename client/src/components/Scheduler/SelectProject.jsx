ff5209229814178d148be88c61dfcff9ea9db185
Vivian Zhang <vvynz12@gmail.com>
2022-03-25 17:20:02 -0400

    Set up list menu

--- client/src/components/Scheduler/SelectProject.jsx
+++ client/src/components/Scheduler/SelectProject.jsx
@@ -4,6 +4,14 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+
+
 
 export default function SelectProject() {
   const [project, setProject] = React.useState('');
@@ -13,21 +21,28 @@ export default function SelectProject() {
   };
 
   return (
-    <Box sx={{ minWidth: 175 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Select Project</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={project}
-          label="Select Project"
-          onChange={handleChange}
-        >
-          <MenuItem value={'projectID1'}>Project Name #1</MenuItem>
-          <MenuItem value={'projectID2'}>Project Name #2</MenuItem>
-          <MenuItem value={'projectID3'}>Project Name #3</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+    // <Box sx={{ minWidth: 175 }}>
+    //   <FormControl fullWidth>
+    //     <InputLabel id="demo-simple-select-label">Select Project</InputLabel>
+    //     <Select
+    //       labelId="demo-simple-select-label"
+    //       id="demo-simple-select"
+    //       value={project}
+    //       label="Select Project"
+    //       onChange={handleChange}
+    //     >
+    //       <MenuItem value={'projectID1'}>Project Name #1</MenuItem>
+    //       <MenuItem value={'projectID2'}>Project Name #2</MenuItem>
+    //       <MenuItem value={'projectID3'}>Project Name #3</MenuItem>
+    //     </Select>
+    //   </FormControl>
+    // </Box>
+
+
+    <List>
+      <ListItemButton>
+        <ListItemText primary="Select Project"/>
+      </ListItemButton>
+    </List>
   );
 }
