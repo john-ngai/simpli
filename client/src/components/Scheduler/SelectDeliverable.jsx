f15bc09bf4986d5801289ea05fa55e652c7cebd8
Vivian Zhang <vvynz12@gmail.com>
2022-03-25 21:41:42 -0400

    pass in props and reorganize code

--- client/src/components/Scheduler/SelectDeliverable.jsx
+++ client/src/components/Scheduler/SelectDeliverable.jsx
@@ -1,16 +1,26 @@
 import * as React from 'react';
 import useAppData from '../../hooks/useAppData';
-import { ListItemButton, ListItemText } from '@mui/material';
+import { List, ListItemButton, ListItemText } from '@mui/material';
 
-export default function SelectDeliverable() {
+export default function SelectDeliverable(props) {
   const {state} = useAppData();
+  const {onClick} = props;
 
-  const delListItem = {
-    <delListItem />;
-  }
+  const delList = props.deliverables.map(deliverable => {
+    <delListItem 
+    key={deliverable.id} 
+    id={deliverable.id} 
+    name={deliverable.name} 
+    setDeliverable={onClick} 
+    />;
+  });
+
   return (
-    <ListItemButton>
-
-    </ListItemButton>
+    <List sx={{ width: 'auto' }} >
+      {delList}
+    </List>
+    // <ListItemButton>
+    //   <ListItemText primary={name} />
+    // </ListItemButton>
   );
 }
