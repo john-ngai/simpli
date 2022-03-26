71898cb15777aa2b0e343cc05bf1e84ea3e5c7e2
Vivian Zhang <vvynz12@gmail.com>
2022-03-26 13:19:11 -0400

    Move some lines of code to SelectDelListItem to fix the bug of having more than 1 deliverable selected at a time

--- client/src/components/Scheduler/SelectDelListItem.jsx
+++ client/src/components/Scheduler/SelectDelListItem.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
 
 export default function SelectDelListItem(props) {
   const { id, name, setDeliverable } = props;
+
+  const [open, setOpen] = useState(true);
+  const handleOpen = () => {
+    setOpen(!open);
+  }
+
   return (
-    <ListItemButton>
-      <ListItemText 
-      sx={{
-        bgcolor: "lightgrey"
-      }}
+    <ListItemButton key={id} id={id} onClick={handleOpen} >
+      <ListItemText
       primary={name}
       primaryTypographyProps={{
         fontSize: 12
       }}
       onClick={()=>{setDeliverable(id)}} 
       />
+      {!open ? <ExpandMore /> : <ExpandLess />}
     </ListItemButton>
   );
 }
--- client/src/components/Scheduler/SelectDeliverable.jsx
+++ client/src/components/Scheduler/SelectDeliverable.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import useAppData from '../../hooks/useAppData';
 import SelectDelListItem from './SelectDelListItem';
-import { List, ListItemButton, ListItemText, ListSubheader } from '@mui/material';
+import { List, ListItemButton, ListItemText, ListSubheader, Collapse } from '@mui/material';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 
@@ -14,31 +14,34 @@ export default function SelectDeliverable(props) {
     setOpen(!open);
   }
 
-  // const delList = props.deliverables.map(deliverable => 
-  //   <SelectDelListItem 
-  //   key={deliverable.id} 
-  //   id={deliverable.id} 
-  //   name={deliverable.name} 
-  //   setDeliverable={onChange} 
-  //   />
-  // );
-
-  const delList = props.deliverables.map(deliverable =>
-    <ListItemButton key={deliverable.id} id={deliverable.id} onClick={handleOpen} >
-      <ListItemText 
-      primary={deliverable.name}
-      primaryTypographyProps={{
-        fontSize: 12
-      }}
-      onClick={()=>{onChange(deliverable.id)}} 
-      />
-    </ListItemButton>
+  const delList = props.deliverables.map(deliverable => 
+    <SelectDelListItem 
+    key={deliverable.id} 
+    id={deliverable.id} 
+    name={deliverable.name} 
+    setDeliverable={onChange} 
+    />
   );
 
+  // const delList = props.deliverables.map(deliverable =>
+  //   <ListItemButton key={deliverable.id} id={deliverable.id} onClick={handleOpen} >
+  //     <ListItemText 
+  //     primary={deliverable.name}
+  //     primaryTypographyProps={{
+  //       fontSize: 12
+  //     }}
+  //     onClick={()=>{onChange(deliverable.id)}} 
+  //     />
+  //     {!open ? <ExpandMore /> : <ExpandLess />}
+  //   </ListItemButton>
+  // );
+
   return (
+  <Collapse in={open} timeout={"auto"} unmountOnExit>
   <List sx={{ width: 'auto'}} subheader={
     <ListSubheader sx={{ textDecoration: "underline", '&:hover': { backgroundColor: "lightgray" } }} >Deliverables</ListSubheader> } >
     {delList}
-  </List>    
+  </List>
+  </Collapse>   
   )
 }
