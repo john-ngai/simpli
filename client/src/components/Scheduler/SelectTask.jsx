90eb16e1601fad2b723b08d4253196deaceafe65
Vivian Zhang <vvynz12@gmail.com>
2022-03-26 14:08:42 -0400

    Refactor code and pass down props to SelectTaskListItem

--- client/src/components/Scheduler/SelectTask.jsx
+++ client/src/components/Scheduler/SelectTask.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
+import SelectTaskListItem from './SelectTaskListItem';
 import { List, ListItemButton, ListItemText, ListSubheader } from '@mui/material';
 
 export default function SelectTask(props) {
   const {onChange} = props;
 
-  const taskList = props.tasks.map(task => 
-    <ListItemButton key={task.id} id={task.id} >
-      <ListItemText 
-      primary={task.name} 
-      onClick={() => {onChange(task.id)}}
-      />
-    </ListItemButton>
+  const taskList = props.tasks.map(task =>
+    <SelectTaskListItem 
+    key={task.id}
+    id={task.id}
+    name={task.name}
+    setTask={()=>{onChange(task.id)}}
+    />
     );
+
+  // const taskList = props.tasks.map(task => 
+  //   <ListItemButton key={task.id} id={task.id} >
+  //     <ListItemText 
+  //     primary={task.name} 
+  //     onClick={() => {onChange(task.id)}}
+  //     />
+  //   </ListItemButton>
+  //   );
     
   return(
     <List sx={{ width: 'auto'}} subheader={
       <ListSubheader sx={{ textDecoration: "underline", '&:hover': { backgroundColor: "lightgray" } }} >Tasks</ListSubheader>
-    }>
-
+    } >
+      {taskList}
     </List>
   )
 }
