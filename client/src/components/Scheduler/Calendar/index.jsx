6a0e30fcc3aade10fa3af54540656a62cdc762ea
mrjohnming <john.ngai@hotmail.com>
2022-03-26 16:35:01 -0400

    Remove test code

--- client/src/components/Scheduler/Calendar/index.jsx
+++ client/src/components/Scheduler/Calendar/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import useAppData from '../../../hooks/useAppData';
-import './Calendar.scss';
+import './index.scss';
 // Components
 import _7AM from './_7AM';
 import _8AM from './_8AM';
@@ -25,125 +25,7 @@ const helper = require('./helpers');
 
 export default function Calendar() {
   const { state } = useAppData();
-  // const schedule = state.schedule;
-
-
   const schedule = state.schedule;
-  console.log(schedule);
-
-  // Temp data
-  /*const schedule = {
-    1: {
-      id: 1,
-      start_time: "7am",
-      end_time: "8am",
-      day_id: 1,
-      task: {
-        id: 2,
-        name: "Clean the entire house",
-        description: "Place valuables somewhere safe"
-      }
-    },
-    13: {
-      id: 13,
-      start_time: "4pm",
-      end_time: "7pm",
-      day_id: 1,
-      task: {
-        id: 21,
-        name: "Some other task",
-        description: "Some other description"
-      }
-    },
-    2: {
-      id: 2,
-      start_time: "9am",
-      end_time: "12pm",
-      day_id: 2,
-      task: {
-        id: 2,
-        name: "Purchase industrial vacuum",
-        description: "Random description 1"
-      }
-    },
-    3: {
-      id: 3,
-      start_time: "4pm",
-      end_time: "6pm",
-      day_id: 3,
-      task: {
-        id: 2,
-        name: "Visit Sifu",
-        description: "Random description 1"
-      }
-    },
-    4: {
-      id: 4,
-      start_time: "11am",
-      end_time: "12pm",
-      day_id: 4,
-      task: {
-        id: 2,
-        name: "Browse for jobs near the house",
-        description: "Random description 1"
-      }
-    },
-    5: {
-      id: 5,
-      start_time: "10am",
-      end_time: "11p  m",
-      day_id: 5,
-      task: {
-        id: 2,
-        name: "Research best dog training guides",
-        description: "Random description 1"
-      }
-    },
-    6: {
-      id: 6,
-      start_time: "7am",
-      end_time: "2pm",
-      day_id: 6,
-      task: {
-        id: 2,
-        name: "Calculate corporate HST refund",
-        description: "Random description 1"
-      }
-    },
-    7: {
-      id: 7,
-      start_time: "9am",
-      end_time: "11am",
-      day_id: 7,
-      task: {
-        id: 2,
-        name: "Shortlist new dinner recipes",
-        description: "Random description 1"
-      }
-    },
-    8: {
-      id: 8,
-      start_time: "1pm",
-      end_time: "4pm",
-      day_id: 7,
-      task: {
-        id: 3,
-        name: "Shortlist new dinner recipes",
-        description: "Random description 1"
-      }
-    },
-    9: {
-      id: 9,
-      start_time: "6pm",
-      end_time: "8pm",
-      day_id: 7,
-      task: {
-        id: 4,
-        name: "Eat your ABCDs",
-        description: "Random description 1"
-      }
-    }
-  };/**/
   
   return (
     <section id="calendar">
