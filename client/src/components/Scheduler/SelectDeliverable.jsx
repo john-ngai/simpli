0c6ecd68d3773440c1de3f1c41b925ef744020cd
Vivian Zhang <vvynz12@gmail.com>
2022-03-26 12:08:28 -0400

    Fix bug

--- client/src/components/Scheduler/SelectDeliverable.jsx
+++ client/src/components/Scheduler/SelectDeliverable.jsx
@@ -5,25 +5,25 @@ import { List, ListItemButton, ListItemText } from '@mui/material';
 
 export default function SelectDeliverable(props) {
   const {state} = useAppData();
-  const {onClick, setDeliverable} = props;
+  const {onChange, setDeliverable} = props;
 
-  // const delList = props.deliverables.map(deliverable => {
-  //   <SelectDelListItem 
-  //   key={deliverable.id} 
-  //   id={deliverable.id} 
-  //   name={deliverable.name} 
-  //   setDeliverable={onClick} 
-  //   />;
-  // });
+  const delList = props.deliverables.map(deliverable => 
+    <SelectDelListItem 
+    key={deliverable.id} 
+    id={deliverable.id} 
+    name={deliverable.name} 
+    setDeliverable={onChange} 
+    />
+  );
 
-  const delList = props.deliverables.map(deliverable => {
-    <ListItemButton key={deliverable.id} id={deliverable.id} >
-      <ListItemText 
-      primary={deliverable.name}
-      onClick={()=>{setDeliverable(deliverable.id)}} 
-      />
-    </ListItemButton>
-  })
+  // const delList = props.deliverables.map(deliverable =>
+  //   <ListItemButton key={deliverable.id} id={deliverable.id} >
+  //     <ListItemText 
+  //     primary={deliverable.name}
+  //     onClick={()=>{setDeliverable(deliverable.id)}} 
+  //     />
+  //   </ListItemButton>
+  // );
 
   return (
   <List sx={{ width: 'auto' }} >
