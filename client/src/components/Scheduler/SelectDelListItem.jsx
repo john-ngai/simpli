ca463dbb5d61a2f51d2fecab474651138901c2c5
Vivian Zhang <vvynz12@gmail.com>
2022-03-25 21:44:00 -0400

    Createa a SelectDelListItem

---
+++ client/src/components/Scheduler/SelectDelListItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+export default function SelectDelListItem(props) {
+  return (
+    <ListItemButton>
+      <ListItemText />
+    </ListItemButton>
+  );
+}
