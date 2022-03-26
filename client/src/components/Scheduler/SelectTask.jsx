54e6d0bd2569a4f5cde31cfb00461ef3a0fd96a4
Vivian Zhang <vvynz12@gmail.com>
2022-03-26 12:46:55 -0400

    Create a SelectTask component

---
+++ client/src/components/Scheduler/SelectTask.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { List, ListItemButton, ListItemText } from '@mui/material';
+
+export default function SelectTask() {
+  return(
+    <List>
+      
+    </List>
+  )
+}
