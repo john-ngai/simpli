d14354d054c9f8ea193426d45fc2d09b2b1803d3
Vivian Zhang <vvynz12@gmail.com>
2022-03-26 10:38:28 -0400

    remove unused code

--- client/src/components/Scheduler/SelectDeliverable.jsx
+++ client/src/components/Scheduler/SelectDeliverable.jsx
@@ -7,21 +7,19 @@ export default function SelectDeliverable(props) {
   const {state} = useAppData();
   const {onClick} = props;
 
-  const delList = props.deliverables.map(deliverable => {
-    <SelectDelListItem 
-    key={deliverable.id} 
-    id={deliverable.id} 
-    name={deliverable.name} 
-    setDeliverable={onClick} 
-    />;
-  });
+  // const delList = props.deliverables.map(deliverable => {
+  //   <SelectDelListItem 
+  //   key={deliverable.id} 
+  //   id={deliverable.id} 
+  //   name={deliverable.name} 
+  //   setDeliverable={onClick} 
+  //   />;
+  // });
 
   return (
     <List sx={{ width: 'auto' }} >
       {delList}
     </List>
-    // <ListItemButton>
-    //   <ListItemText primary={name} />
-    // </ListItemButton>
+    
   );
 }
