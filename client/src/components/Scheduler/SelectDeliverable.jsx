6566b894326de714b56a4d01365a7b460f3d03ec
Vivian Zhang <vvynz12@gmail.com>
2022-03-26 14:24:02 -0400

    Remove unused code

--- client/src/components/Scheduler/SelectDeliverable.jsx
+++ client/src/components/Scheduler/SelectDeliverable.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import useAppData from '../../hooks/useAppData';
 import SelectDelListItem from './SelectDelListItem';
-import { List, ListItemButton, ListItemText, ListSubheader, Collapse } from '@mui/material';
+import { List, ListSubheader, Collapse } from '@mui/material';
 
 export default function SelectDeliverable(props) {
-  const {state} = useAppData();
   const {onChange, transition} = props;
 
   const [open, setOpen] = useState(true);
@@ -21,19 +20,6 @@ export default function SelectDeliverable(props) {
     }} 
     />
   );
-
-  // const delList = props.deliverables.map(deliverable =>
-  //   <ListItemButton key={deliverable.id} id={deliverable.id} onClick={handleOpen} >
-  //     <ListItemText 
-  //     primary={deliverable.name}
-  //     primaryTypographyProps={{
-  //       fontSize: 12
-  //     }}
-  //     onClick={()=>{onChange(deliverable.id)}} 
-  //     />
-  //     {!open ? <ExpandMore /> : <ExpandLess />}
-  //   </ListItemButton>
-  // );
 
   return (
   <Collapse in={open} timeout="auto" unmountOnExit>
--- client/src/components/Scheduler/SelectTask.jsx
+++ client/src/components/Scheduler/SelectTask.jsx
@@ -13,15 +13,6 @@ export default function SelectTask(props) {
     setTask={()=>{onChange(task.id)}}
     />
     );
-
-  // const taskList = props.tasks.map(task => 
-  //   <ListItemButton key={task.id} id={task.id} >
-  //     <ListItemText 
-  //     primary={task.name} 
-  //     onClick={() => {onChange(task.id)}}
-  //     />
-  //   </ListItemButton>
-  //   );
     
   return(
     <List sx={{ width: 'auto'}} subheader={
