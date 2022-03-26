a73daa005bdeb9314d9f56a9221ba1eaaf28d39c
Vivian Zhang <vvynz12@gmail.com>
2022-03-26 12:46:19 -0400

    Add some styling to the list subheader

--- client/src/components/Scheduler/SelectDeliverable.jsx
+++ client/src/components/Scheduler/SelectDeliverable.jsx
@@ -1,32 +1,37 @@
 import * as React from 'react';
 import useAppData from '../../hooks/useAppData';
 import SelectDelListItem from './SelectDelListItem';
-import { List, ListItemButton, ListItemText } from '@mui/material';
+import { List, ListItemButton, ListItemText, ListSubheader } from '@mui/material';
 
 export default function SelectDeliverable(props) {
   const {state} = useAppData();
-  const {onChange, setDeliverable} = props;
+  const {onChange} = props;
 
-  const delList = props.deliverables.map(deliverable => 
-    <SelectDelListItem 
-    key={deliverable.id} 
-    id={deliverable.id} 
-    name={deliverable.name} 
-    setDeliverable={onChange} 
-    />
-  );
-
-  // const delList = props.deliverables.map(deliverable =>
-  //   <ListItemButton key={deliverable.id} id={deliverable.id} >
-  //     <ListItemText 
-  //     primary={deliverable.name}
-  //     onClick={()=>{setDeliverable(deliverable.id)}} 
-  //     />
-  //   </ListItemButton>
+  // const delList = props.deliverables.map(deliverable => 
+  //   <SelectDelListItem 
+  //   key={deliverable.id} 
+  //   id={deliverable.id} 
+  //   name={deliverable.name} 
+  //   setDeliverable={onChange} 
+  //   />
   // );
 
+  const delList = props.deliverables.map(deliverable =>
+    <ListItemButton key={deliverable.id} id={deliverable.id} >
+      <ListItemText 
+      primary={deliverable.name}
+      primaryTypographyProps={{
+        fontSize: 12
+      }}
+      onClick={()=>{onChange(deliverable.id)}} 
+      />
+    </ListItemButton>
+  );
+
   return (
-  <List sx={{ width: 'auto' }} >
+  <List sx={{ width: 'auto'}} subheader={
+    <ListSubheader sx={{ textDecoration: "underline", '&:hover': { backgroundColor: "lightgray" } }} >Deliverables</ListSubheader>
+  } >
     {delList}
   </List>    
   )
