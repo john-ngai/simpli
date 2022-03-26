5b81fc25c8144348712921889d65b2a06c8f1033
Vivian Zhang <vvynz12@gmail.com>
2022-03-25 22:18:04 -0400

    Pass the handleOpen function to the ListItemButton and render the ExandMore or ExandLess icons

--- client/src/components/Scheduler/SelectProjectItem.jsx
+++ client/src/components/Scheduler/SelectProjectItem.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
 
 export default function SelectProjectItem(props) {
   const {id, name, setProject} = props;
+
+  const handleOpen = () => {
+    setOpen(!open);
+  }
+  const [open, setOpen] = React.useState(true);
+
   return (
-    <ListItemButton>
+    <ListItemButton onClick={handleOpen}> 
       <ListItemText 
       id={id} 
       primary={name} 
@@ -14,6 +22,7 @@ export default function SelectProjectItem(props) {
         fontSize: 14
       }}
       onClick={setProject} />
+      {!open ? <ExpandMore /> : <ExpandLess />}
     </ListItemButton>
   );
 }
