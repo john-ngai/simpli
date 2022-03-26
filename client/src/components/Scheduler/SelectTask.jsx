21987a087b9e4920a3750b78f0b0b25fefdde40c
Vivian Zhang <vvynz12@gmail.com>
2022-03-26 13:40:29 -0400

    Create a SelectTaskListItem component

--- client/src/components/Scheduler/SelectTask.jsx
+++ client/src/components/Scheduler/SelectTask.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { List, ListItemButton, ListItemText } from '@mui/material';
+import { List, ListItemButton, ListItemText, ListSubheader } from '@mui/material';
 
-export default function SelectTask() {
+export default function SelectTask(props) {
+  const {onChange} = props;
+
+  const taskList = props.tasks.map(task => 
+    <ListItemButton key={task.id} id={task.id} >
+      <ListItemText 
+      primary={task.name} 
+      onClick={() => {onChange(task.id)}}
+      />
+    </ListItemButton>
+    );
+    
   return(
-    <List>
-      
+    <List sx={{ width: 'auto'}} subheader={
+      <ListSubheader sx={{ textDecoration: "underline", '&:hover': { backgroundColor: "lightgray" } }} >Tasks</ListSubheader>
+    }>
+
     </List>
   )
 }
---
+++ client/src/components/Scheduler/SelectTaskListItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+export default function SelectTaskListItem(props) {
+  return(
+    <ListItemButton>
+      <ListItemText />
+    </ListItemButton>
+  )
+}
