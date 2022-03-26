7e3890a85bfc544085c3b597997bb949535dbb97
Neale Taylor <nealeptaylor@gmail.com>
2022-03-26 16:39:44 -0400

    remove task selector from sidebar

--- client/src/components/Scheduler/index.jsx
+++ client/src/components/Scheduler/index.jsx
@@ -1,38 +1,26 @@
 // import React, { Fragment } from 'react';
-import { React, useState, useMemo, Fragment } from 'react';
+import { React, useState } from 'react';
 import './index.scss';
 // Components
 import NavBar from '../NavBar';
-import SelectProject from './SelectProject';
 import Calendar from './Calendar';
 import PopupForm from './Form';
-import SelectDeliverable from './SelectDeliverable';
-import SelectTask from './SelectTask';
+
 // Material-UI
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-import { List, ListItemButton, ListItemText, Collapse } from '@mui/material';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
 // Hooks
 import useAppData from '../../hooks/useAppData';
 import useVisualMode from '../../hooks/useVisualMode';
 
-const DELIVERABLES = "DELIVERABLES";
-const TASKS = "TASKS";
 
 export default function Scheduler(props) {
   let user = null;
   const [openForm, setOpenForm] = useState(false);
   const handleOpenForm = () => setOpenForm(!openForm);
-  const { state, setProject, getSelectedProject, getDeliverables, setDeliverable, getSelectedDeliverable, setTask, getTasks, getSelectedTask } = useAppData();
-  const {mode, transition} = useVisualMode(null);
-  
+  const { state, getSelectedProject, getDeliverables, getSelectedDeliverable, getTasks, getSelectedTask } = useAppData();
+
   const [open, setOpen] = useState(true);
 
-  const selectedProject = getSelectedProject(state);
-  const deliverables = getDeliverables(state, state.project);
-  const selectedDel = getSelectedDeliverable(state);
-  const tasks = getTasks(state, state.deliverable);
   const selectedTask = getSelectedTask(state);
 
   if (!localStorage.user) {
@@ -58,56 +46,7 @@ export default function Scheduler(props) {
 
         <aside id="menu">
           <br />
-          <List sx={{ width: '100%', maxWidth: 300 }}>
-            <ListItemButton onClick={handleOpen}>
-              <ListItemText 
-              primary="Select Project" 
-              primaryTypographyProps={{
-              color: 'primary',
-              fontWeight: 'bold'
-              }} 
-              />
 
-            {open ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <SelectProject 
-              projects={Object.values(state.projects)} 
-              // value={state.project} 
-              onChange={setProject}
-              onClick={handleOpen}
-              transition={transition}
-              />
-              {/* <Collapse in={open} timeout="auto" unmountOnExit> */}
-                { mode === DELIVERABLES && <SelectDeliverable
-                deliverables={deliverables}
-                onChange={setDeliverable} 
-                selectedDel={selectedDel}
-                selectedProject={selectedProject}
-                transition={transition}
-                /> }
-              {/* </Collapse> */}
-              { mode === TASKS && 
-              <Fragment>
-              <SelectDeliverable
-              deliverables={deliverables}
-              onChange={setDeliverable} 
-              selectedDel={selectedDel}
-              selectedProject={selectedProject}
-              transition={transition}
-              /> 
-
-              <SelectTask 
-              tasks={tasks}
-              onChange={setTask}
-              selectedProject={selectedProject}
-              selectedDel={selectedDel}
-              selectedTask={selectedTask}
-              /> 
-              </Fragment>
-              }
-            </Collapse>
-          </List>
           <br />
           <span><strong>Completed</strong></span>
           <br /><br />
@@ -122,7 +61,6 @@ export default function Scheduler(props) {
           <PopupForm 
           openForm={openForm}
           handleOpenForm={handleOpenForm}
-          selectedTask={selectedTask}
           />
         </div>
         </aside>
