70090efeb4d3bdb1e7ece5ed142721e25f45dc1d
Vivian Zhang <vvynz12@gmail.com>
2022-03-27 12:57:38 -0400

    Change styling of project name and fix spacing between the nav items

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,6 +5,7 @@ import "./NavBar.css";
 export default function Navbar(props) {
   return (
     <nav id="navbar">
+      <div className="nav_wrapper">
       <span id="app_name">Simpli</span>
       <span className="nav_item">
         <Link to="/">Summary</Link>
@@ -13,12 +14,13 @@ export default function Navbar(props) {
         {/* <Link to="/schedule">Schedule</Link> */}
         <Link to="/scheduler">Scheduler</Link>
       </span>
+      </div>
       <span className="login_nav">
         {!props.user && <Link to="/register">Register</Link>}
         {!props.user && <Link to="/login">Login</Link>}
-        {props.user && <a href="" onClick={() => localStorage.removeItem('user')}>Logout</a>}
+        {props.user && <a href="" id="login_nav_item" onClick={() => localStorage.removeItem('user')}>Logout</a>}
+        {props.user && <span id="login_nav_item">Logged in as: {props.user}</span> }
       </span>
-      {props.user && <span className="nav_item">Logged in as: {props.user}</span> }
     </nav>
   )
 }
