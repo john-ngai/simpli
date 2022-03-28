5609c6614e47f864f222f0d69bb1f6ef6d0b46ac
mrjohnming <john.ngai@hotmail.com>
2022-03-28 10:47:04 -0400

    Add columns to projects - completed_deliverables & total_deliverables

--- server/db/seeds/03_seeds_projects.sql
+++ server/db/seeds/03_seeds_projects.sql
@@ -1,3 +1,8 @@
-INSERT INTO projects (name, description, team_id) VALUES ('Spring Cleaning', 'Clean-up all the bedrooms', 1);
-INSERT INTO projects (name, description, team_id) VALUES ('Create a project tracking web app', 'Lighthouse Labs final project', 1);
-INSERT INTO projects (name, description, team_id) VALUES ('Learn React Native', '', 1);
+INSERT INTO projects (name, description, completed_deliverables, total_deliverables, team_id)
+  VALUES ('Spring Cleaning', 'Clean-up all the bedrooms', 0, 0, 1);
+
+INSERT INTO projects (name, description, completed_deliverables, total_deliverables, team_id)
+  VALUES ('Create a project tracking web app', 'Lighthouse Labs final project', 0, 0, 1);
+
+INSERT INTO projects (name, description, completed_deliverables, total_deliverables, team_id)
+  VALUES ('Learn React Native', '', 0, 0, 1);
