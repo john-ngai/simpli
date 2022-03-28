9a9e58a9364f0a373a5a069535f1f8ccc13d899c
Vivian Zhang <vvynz12@gmail.com>
2022-03-28 16:48:43 -0400

    Minor update

--- client/src/Teams.jsx
+++ client/src/Teams.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 export default function Teams(props) {
   return(
-    <table>
+ 
       <tbody>
         <tr>
           <th>{props.teamID}</th>
           <th>{props.name}</th>
         </tr>
       </tbody>
-    </table>
   )
 }
