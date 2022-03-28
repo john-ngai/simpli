cc0fdc8c199b2e0b85c2ee7787030a3ab290edec
Vivian Zhang <vvynz12@gmail.com>
2022-03-28 17:14:31 -0400

    Setup token verification

--- server/routes/teams.js
+++ server/routes/teams.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 
 module.exports = (db) => {
-  // all routes will go here
+  
+  const token = req.headers['x-access-token'];
+
+  // if no token is found, return an empty object
+  if (!token) {
+    return res.send({});
+  }
+
   router.get('/', (req, res) => {
     const command = "SELECT * FROM teams";
     db.query(command).then(data => {
