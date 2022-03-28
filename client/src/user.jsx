705d25675e691d3beed0eae739644d4d35b19f53
Vivian Zhang <vvynz12@gmail.com>
2022-03-28 15:48:29 -0400

    Import Teams component

--- client/src/user.jsx
+++ client/src/user.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Teams from './Teams';
 import './App.scss';
 
 export default function User() {
+  console.log("user??", JSON.parse(localStorage.user));
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+      <Teams />
+    </div>
   )
 }
