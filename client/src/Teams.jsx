dfc3a8a96d1738565a1c8858e0764fdaa09f087c
Vivian Zhang <vvynz12@gmail.com>
2022-03-28 16:47:44 -0400

    Move the table head elements from Teams.jsx to user.jsx

--- client/src/Teams.jsx
+++ client/src/Teams.jsx
@@ -3,12 +3,6 @@ import React from 'react';
 export default function Teams(props) {
   return(
     <table>
-      <thead>
-        <tr>
-          <th>Team ID</th>
-          <th>Name</th>
-        </tr>
-      </thead>
       <tbody>
         <tr>
           <th>{props.teamID}</th>
--- client/src/user.jsx
+++ client/src/user.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Teams from './Teams';
-import './App.scss';
+import React from "react";
+import Teams from "./Teams";
+import "./App.scss";
 
 export default function User() {
   // console.log("user??", JSON.parse(localStorage.user));
@@ -14,47 +14,47 @@ export default function User() {
       name: "Rick Sanchez",
       email: "rick.sandchez@gmail.com",
       password: "picklerick",
-      team_id: 1
-      },
-      {
+      team_id: 1,
+    },
+    {
       id: 2,
       name: "Lisa Simpson",
       email: "lisa.simpson@gmail.com",
       password: "ehhhhh",
-      team_id: 2
-      },
-      {
+      team_id: 2,
+    },
+    {
       id: 3,
       name: "Link",
       email: "link@yahoo.com",
       password: "hyrule",
-      team_id: 1
-      },
-      {
+      team_id: 1,
+    },
+    {
       id: 4,
       name: "Simon Bell",
       email: "simon_bel123@mail.ca",
       password: "dracula",
-      team_id: 3
-      },
-      {
+      team_id: 3,
+    },
+    {
       id: 5,
       name: "Ultron",
       email: "all_might@academia.jp",
       password: "plusUltra",
-      team_id: 1
-      },
-      {
+      team_id: 1,
+    },
+    {
       id: 6,
       name: "Mario Marucci",
       email: "mario@mushroomkindom.jp",
       password: "plumber79",
-      team_id: 1
-      }
+      team_id: 1,
+    },
   ];
   // TEST CODE
 
-  const getUsers = (users, currentUser)=> {
+  const getUsers = (users, currentUser) => {
     const allUsers = Object.values(users);
     const selectedUsers = [];
     const teamID = currentUser.team_id;
@@ -65,24 +65,28 @@ export default function User() {
       }
     }
     return selectedUsers;
-  }
+  };
 
   const team = getUsers(users, currentUser);
 
   console.log("TESTING", team);
 
-  const teamList = team.map(user => 
-    <Teams 
-     key={user.id}
-     teamID={user.team_id}
-     name={user.name}
-     />
-    );
-  
+  const teamList = team.map((user) => (
+    <Teams key={user.id} teamID={user.team_id} name={user.name} />
+  ));
+
   return (
     <div>
       <h1>Hello World</h1>
-      {teamList}
+      <table>
+        <thead>
+          <tr>
+            <th>Team ID</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        {teamList}
+      </table>
     </div>
-  )
+  );
 }
