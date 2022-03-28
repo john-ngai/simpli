432e1bd50de01954225d0a4af3669c8b9bb7b6b1
Vivian Zhang <vvynz12@gmail.com>
2022-03-28 16:13:57 -0400

    setup table to display user's team

--- client/src/Teams.jsx
+++ client/src/Teams.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 
 export default function Teams() {
   return(
-    <h3>HELLO</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>1</th>
+          <th>Tony Stark</th>
+        </tr>
+      </tbody>
+    </table>
   )
 }
