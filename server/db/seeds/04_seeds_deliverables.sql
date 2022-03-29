74ee923c5e23314456d73a7157537aa8ad67df33
Neale Taylor <nealeptaylor@gmail.com>
2022-03-29 12:28:13 -0400

    add columns to deliverables tables

--- server/db/seeds/04_seeds_deliverables.sql
+++ server/db/seeds/04_seeds_deliverables.sql
@@ -1,8 +1,8 @@
-INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Clean master bedroom', '', false, false, 1);
-INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Clean other bedrooms', 'Start with main floor', false, true, 1);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Clean master bedroom', '', false, false, 0, 1, 1);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Clean other bedrooms', 'Start with main floor', false, false, 0, 2, 1);
 
-INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Create a plan', 'User stories, routes, stacks, etc', FALSE, false, 2);
-INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Setup project folder', 'Front-end, back-end, & database', FALSE, false, 2);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Create a plan', 'User stories, routes, stacks, etc', FALSE, false, 0, 3, 2);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Setup project folder', 'Front-end, back-end, & database', FALSE, false, 0, 3, 2);
 
-INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Research best courses', 'YouTube, Udemy, Books, etc.', FALSE, false, 3);
-INSERT INTO deliverables (name, description, priority, status, project_id) VALUES ('Create a self-learning schedule', '', FALSE, false, 3);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Research best courses', 'YouTube, Udemy, Books, etc.', FALSE, false, 0, 1, 3);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Create a self-learning schedule', '', FALSE, false, 0, 1, 3);
