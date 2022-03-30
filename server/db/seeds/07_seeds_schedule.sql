c13fb1ac17fa22ea8578058364af13191ea9a3a0
mrjohnming <john.ngai@hotmail.com>
2022-03-30 15:55:58 -0400

    Replace old seeds

--- server/db/seeds/07_seeds_schedule.sql
+++ server/db/seeds/07_seeds_schedule.sql
@@ -1,33 +1,31 @@
-INSERT INTO schedule (day_id, start_time, end_time, task_id)
-  VALUES (1, '7am', '7am', 1);
+-- 1 Sun  2 Mon  3 Tues  4 Wed  5 Thurs  6 Fri  7 Sat
 
 INSERT INTO schedule (day_id, start_time, end_time, task_id)
-  VALUES (1, '8am', '10am', 2);
+  VALUES (5, '3pm', '4pm', 25);
 
 INSERT INTO schedule (day_id, start_time, end_time, task_id)
-  VALUES (2, '8am', '12pm', 3);
+  VALUES (5, '4pm', '6pm', 26);
 
 INSERT INTO schedule (day_id, start_time, end_time, task_id)
-  VALUES (3, '11am', '1pm', 4);
+  VALUES (3, '11am', '2pm', 24);
 
 INSERT INTO schedule (day_id, start_time, end_time, task_id)
-  VALUES (3, '2pm', '4pm', 5);
+  VALUES (2, '10am', '12pm', 31);
 
 INSERT INTO schedule (day_id, start_time, end_time, task_id)
-  VALUES (4, '8am', '6pm', 6);
+  VALUES (2, '1pm', '6pm', 34);
 
 INSERT INTO schedule (day_id, start_time, end_time, task_id)
-  VALUES (5, '9am', '11am', 7);
+  VALUES (1, '10am', '12pm', 1);
 
 INSERT INTO schedule (day_id, start_time, end_time, task_id)
-  VALUES (5, '12pm', '1pm', 8);
+  VALUES (1, '1pm', '2pm', 3);
 
 INSERT INTO schedule (day_id, start_time, end_time, task_id)
-  VALUES (6, '3pm', '3pm', 9);
+  VALUES (2, '1pm', '5pm', 4);
 
 INSERT INTO schedule (day_id, start_time, end_time, task_id)
-  VALUES (7, '7am', '12pm', 10);
+  VALUES (6, '11am', '2pm', 5);
 
 INSERT INTO schedule (day_id, start_time, end_time, task_id)
-  VALUES (7, '12pm', '12am', 11);
-
+  VALUES (7, '6pm', '7pm', 19);
