a8741b15ca737bd01e886c41f50d8ca16d566f93
mrjohnming <john.ngai@hotmail.com>
2022-03-30 16:51:31 -0400

    Change schedule item colour to green for completed tasks

--- client/src/components/Scheduler/Calendar/_5PM.jsx
+++ client/src/components/Scheduler/Calendar/_5PM.jsx
@@ -14,57 +14,137 @@ export default function _5PM(props) {
     }
   }
 
+  const completed = sun.completed;
+  console.log('completed =', completed);
+
   return (
     <tr>
       <td className="table time">5 PM</td>
 
-      {sun.name && sun.lastHour ? <td className="table item booked name bottom" onClick={() => edit(sun)}>{sun.name}</td> :
-        sun.name ? <td className="table item booked name" onClick={() => edit(sun)}>{sun.name}</td> :
-          sun.lastHour ? <td className="table item booked bottom" onClick={() => edit(sun)}></td> :
-            sun.id ? <td className="table item booked" onClick={() => edit(sun)}></td> :
-              <td className="table item"></td>
+      {sun.name && sun.lastHour && sun.completed ?
+        <td className="table item completed name bottom" onClick={() => edit(sun)}>{sun.name}</td> : sun.name && sun.lastHour ?
+          <td className="table item booked name bottom" onClick={() => edit(sun)}>{sun.name}</td> :
+
+          sun.name && sun.completed ?
+            <td className="table item completed name" onClick={() => edit(sun)}>{sun.name}</td> : sun.name ?
+              <td className="table item booked name" onClick={() => edit(sun)}>{sun.name}</td> :
+
+              sun.lastHour && sun.completed ?
+                <td className="table item completed bottom" onClick={() => edit(sun)}></td> : sun.lastHour ?
+                  <td className="table item booked bottom" onClick={() => edit(sun)}></td> :
+
+                  sun.id && sun.completed ?
+                    <td className="table item completed" onClick={() => edit(sun)}></td> : sun.id ?
+                      <td className="table item booked" onClick={() => edit(sun)}></td> :
+                      <td className="table item"></td>
       }
 
-      {mon.name && mon.lastHour ? <td className="table item booked name bottom" onClick={() => edit(mon)}>{mon.name}</td> :
-        mon.name ? <td className="table item booked name" onClick={() => edit(mon)}>{mon.name}</td> :
-          mon.lastHour ? <td className="table item booked bottom" onClick={() => edit(mon)}></td> :
-            mon.id ? <td className="table item booked" onClick={() => edit(mon)}></td> :
-              <td className="table item"></td>
+      {mon.name && mon.lastHour && mon.completed ?
+        <td className="table item completed name bottom" onClick={() => edit(mon)}>{mon.name}</td> : mon.name && mon.lastHour ?
+          <td className="table item booked name bottom" onClick={() => edit(mon)}>{mon.name}</td> :
+
+          mon.name && mon.completed ?
+            <td className="table item completed name" onClick={() => edit(mon)}>{mon.name}</td> : mon.name ?
+              <td className="table item booked name" onClick={() => edit(mon)}>{mon.name}</td> :
+
+              mon.lastHour && mon.completed ?
+                <td className="table item completed bottom" onClick={() => edit(mon)}></td> : mon.lastHour ?
+                  <td className="table item booked bottom" onClick={() => edit(mon)}></td> :
+
+                  mon.id && mon.completed ?
+                    <td className="table item completed" onClick={() => edit(mon)}></td> : mon.id ?
+                      <td className="table item booked" onClick={() => edit(mon)}></td> :
+                      <td className="table item"></td>
       }
 
-      {tues.name && tues.lastHour ? <td className="table item booked name bottom" onClick={() => edit(tues)}>{tues.name}</td> :
-        tues.name ? <td className="table item booked name" onClick={() => edit(tues)}>{tues.name}</td> :
-          tues.lastHour ? <td className="table item booked bottom" onClick={() => edit(tues)}></td> :
-            tues.id ? <td className="table item booked" onClick={() => edit(tues)}></td> :
-              <td className="table item"></td>
+      {tues.name && tues.lastHour && tues.completed ?
+        <td className="table item completed name bottom" onClick={() => edit(tues)}>{tues.name}</td> : tues.name && tues.lastHour ?
+          <td className="table item booked name bottom" onClick={() => edit(tues)}>{tues.name}</td> :
+
+          tues.name && tues.completed ?
+            <td className="table item completed name" onClick={() => edit(tues)}>{tues.name}</td> : tues.name ?
+              <td className="table item booked name" onClick={() => edit(tues)}>{tues.name}</td> :
+
+              tues.lastHour && tues.completed ?
+                <td className="table item completed bottom" onClick={() => edit(tues)}></td> : tues.lastHour ?
+                  <td className="table item booked bottom" onClick={() => edit(tues)}></td> :
+
+                  tues.id && tues.completed ?
+                    <td className="table item completed" onClick={() => edit(tues)}></td> : tues.id ?
+                      <td className="table item booked" onClick={() => edit(tues)}></td> :
+                      <td className="table item"></td>
       }
 
-      {wed.name && wed.lastHour ? <td className="table item booked name bottom" onClick={() => edit(wed)}>{wed.name}</td> :
-        wed.name ? <td className="table item booked name" onClick={() => edit(wed)}>{wed.name}</td> :
-          wed.lastHour ? <td className="table item booked bottom" onClick={() => edit(wed)}></td> :
-            wed.id ? <td className="table item booked" onClick={() => edit(wed)}></td> :
-              <td className="table item"></td>
+      {wed.name && wed.lastHour && wed.completed ?
+        <td className="table item completed name bottom" onClick={() => edit(wed)}>{wed.name}</td> : wed.name && wed.lastHour ?
+          <td className="table item booked name bottom" onClick={() => edit(wed)}>{wed.name}</td> :
+
+          wed.name && wed.completed ?
+            <td className="table item completed name" onClick={() => edit(wed)}>{wed.name}</td> : wed.name ?
+              <td className="table item booked name" onClick={() => edit(wed)}>{wed.name}</td> :
+
+              wed.lastHour && wed.completed ?
+                <td className="table item completed bottom" onClick={() => edit(wed)}></td> : wed.lastHour ?
+                  <td className="table item booked bottom" onClick={() => edit(wed)}></td> :
+
+                  wed.id && wed.completed ?
+                    <td className="table item completed" onClick={() => edit(wed)}></td> : wed.id ?
+                      <td className="table item booked" onClick={() => edit(wed)}></td> :
+                      <td className="table item"></td>
       }
 
-      {thurs.name && thurs.lastHour ? <td className="table item booked name bottom" onClick={() => edit(thurs)}>{thurs.name}</td> :
-        thurs.name ? <td className="table item booked name" onClick={() => edit(thurs)}>{thurs.name}</td> :
-          thurs.lastHour ? <td className="table item booked bottom" onClick={() => edit(thurs)}></td> :
-            thurs.id ? <td className="table item booked" onClick={() => edit(thurs)}></td> :
-              <td className="table item"></td>
+      {thurs.name && thurs.lastHour && thurs.completed ?
+        <td className="table item completed name bottom" onClick={() => edit(thurs)}>{thurs.name}</td> : thurs.name && thurs.lastHour ?
+          <td className="table item booked name bottom" onClick={() => edit(thurs)}>{thurs.name}</td> :
+
+          thurs.name && thurs.completed ?
+            <td className="table item completed name" onClick={() => edit(thurs)}>{thurs.name}</td> : thurs.name ?
+              <td className="table item booked name" onClick={() => edit(thurs)}>{thurs.name}</td> :
+
+              thurs.lastHour && thurs.completed ?
+                <td className="table item completed bottom" onClick={() => edit(thurs)}></td> : thurs.lastHour ?
+                  <td className="table item booked bottom" onClick={() => edit(thurs)}></td> :
+
+                  thurs.id && thurs.completed ?
+                    <td className="table item completed" onClick={() => edit(thurs)}></td> : thurs.id ?
+                      <td className="table item booked" onClick={() => edit(thurs)}></td> :
+                      <td className="table item"></td>
       }
 
-      {fri.name && fri.lastHour ? <td className="table item booked name bottom" onClick={() => edit(fri)}>{fri.name}</td> :
-        fri.name ? <td className="table item booked name" onClick={() => edit(fri)}>{fri.name}</td> :
-          fri.lastHour ? <td className="table item booked bottom" onClick={() => edit(fri)}></td> :
-            fri.id ? <td className="table item booked" onClick={() => edit(fri)}></td> :
-              <td className="table item"></td>
+      {fri.name && fri.lastHour && fri.completed ?
+        <td className="table item completed name bottom" onClick={() => edit(fri)}>{fri.name}</td> : fri.name && fri.lastHour ?
+          <td className="table item booked name bottom" onClick={() => edit(fri)}>{fri.name}</td> :
+
+          fri.name && fri.completed ?
+            <td className="table item completed name" onClick={() => edit(fri)}>{fri.name}</td> : fri.name ?
+              <td className="table item booked name" onClick={() => edit(fri)}>{fri.name}</td> :
+
+              fri.lastHour && fri.completed ?
+                <td className="table item completed bottom" onClick={() => edit(fri)}></td> : fri.lastHour ?
+                  <td className="table item booked bottom" onClick={() => edit(fri)}></td> :
+
+                  fri.id && fri.completed ?
+                    <td className="table item completed" onClick={() => edit(fri)}></td> : fri.id ?
+                      <td className="table item booked" onClick={() => edit(fri)}></td> :
+                      <td className="table item"></td>
       }
 
-      {sat.name && sat.lastHour ? <td className="table item booked name bottom" onClick={() => edit(sat)}>{sat.name}</td> :
-        sat.name ? <td className="table item booked name" onClick={() => edit(sat)}>{sat.name}</td> :
-          sat.lastHour ? <td className="table item booked bottom" onClick={() => edit(sat)}></td> :
-            sat.id ? <td className="table item booked" onClick={() => edit(sat)}></td> :
-              <td className="table item"></td>
+      {sat.name && sat.lastHour && sat.completed ?
+        <td className="table item completed name bottom" onClick={() => edit(sat)}>{sat.name}</td> : sat.name && sat.lastHour ?
+          <td className="table item booked name bottom" onClick={() => edit(sat)}>{sat.name}</td> :
+
+          sat.name && sat.completed ?
+            <td className="table item completed name" onClick={() => edit(sat)}>{sat.name}</td> : sat.name ?
+              <td className="table item booked name" onClick={() => edit(sat)}>{sat.name}</td> :
+
+              sat.lastHour && sat.completed ?
+                <td className="table item completed bottom" onClick={() => edit(sat)}></td> : sat.lastHour ?
+                  <td className="table item booked bottom" onClick={() => edit(sat)}></td> :
+
+                  sat.id && sat.completed ?
+                    <td className="table item completed" onClick={() => edit(sat)}></td> : sat.id ?
+                      <td className="table item booked" onClick={() => edit(sat)}></td> :
+                      <td className="table item"></td>
       }
     </tr>
   );
