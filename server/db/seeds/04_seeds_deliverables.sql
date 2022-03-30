876f07358a5cbaafb798ce3a7a07b5b93e61906d
mrjohnming <john.ngai@hotmail.com>
2022-03-30 10:52:59 -0400

    Add seeds for projects #2 & 3

--- server/db/seeds/04_seeds_deliverables.sql
+++ server/db/seeds/04_seeds_deliverables.sql
@@ -1,14 +1,22 @@
--- Project ID #1: Spring Cleaning
+-- Project #1: Spring Cleaning (deliverables #1-5)
 INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Main floor', '', false, false, 1, 6, 1);
 INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('2nd floor', '', false, false, 2, 9, 1);
 INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Basement', 'Donate unused items', false, false, 1, 3, 1);
 INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Garage', '', false, false, 0, 3, 1);
 INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Yard', 'Back to front', false, true, 2, 2, 1);
 
+-- Project #2: Create a Project Tracker Web App (deliverables #6-15)
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Demo presentation', '', true, false, 0, 0, 2);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Client - User page', '', false, false, 0, 0, 2);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Client - Scheduler page', '', false, false, 0, 0, 2);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Client - Register, login, & login pages', '', false, false, 0, 0, 2);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Client - Summary page', '', false, false, 0, 0, 2);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Server - Helper functions', '', false, false, 0, 0, 2);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Server - Routes', '', false, false, 0, 0, 2);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Server - Database', '', false, false, 0, 0, 2);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Setup project folder', '', false, false, 0, 0, 2);
+INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Create a plan', '', false, false, 0, 0, 2);
 
-INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Create a plan', 'User stories, routes, stacks, etc', FALSE, false, 0, 3, 2);
-
-INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Setup project folder', 'Front-end, back-end, & database', FALSE, false, 0, 3, 2);
 
 INSERT INTO deliverables (name, description, priority, status, completed_tasks, total_tasks, project_id) VALUES ('Research best courses', 'YouTube, Udemy, Books, etc.', FALSE, false, 0, 1, 3);
 
