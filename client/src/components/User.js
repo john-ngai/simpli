d5f740ead4b1bb067ae6750dfec65add8226737f
Neale Taylor <nealeptaylor@gmail.com>
2022-03-29 20:14:34 -0400

    tinkering with user display

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -14,35 +14,44 @@ export default function User() {
   const {state, setProject, deleteProject, percentComplete, completedDeliverables, setUser, getUsers} = useAppData();
   const {transition} = useVisualMode();
 
-  console.log(user)
+  // console.log(user)
 
   const team = getUsers(state, user);
+
+  // console.log(team[0].team_code)
 
   const teamList = team.map((member) => (
     <Teams
       key={member.id}
-      teamID={member.team_id}
+      // teamID={member.team_id}
       name={member.name}
       email={member.email}
     />
   ));
 
+  // const teamCode = () => {
+  //   if (team[0].team_code) {
+  //     return team[0].team_code
+  //   } else {
+  //     return "No team code :("
+  //   }
+  // }
   return (
     <div>
       <Navbar user={user.name} />
       <div className="container">
         <div id="user-info">
-          <Avatar sx={{width: 150, height: 150}} src="https://avatars.githubusercontent.com/u/89871393?v=4"/>
+        <Avatar sx={{width: 150, height: 150}} src="https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png"/>
           <h2>{user.name}</h2>
           {user.email}
         </div>
         <div id="team-info">
-          <h2>Team: {user.team_id}</h2>
+          {/* <h2>Team Code: {teamCode} </h2> */}
           <h4>Team members: </h4>
           <table>
         <thead>
           <tr>
-            <th>Team ID</th>
+            {/* <th>Team ID</th> */}
             <th>Name</th>
             <th>Email</th>
           </tr>
@@ -51,10 +60,10 @@ export default function User() {
       </table>
         </div>
       </div>
-      <div className="projects">
+      {/* <div className="projects">
         <h3>Ongoing Projects:</h3>
         {user.team_code}
-      </div>
+      </div> */}
     </div>
   )
 }
